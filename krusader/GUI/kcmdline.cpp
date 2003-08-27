6bc1ff9dad81a0a8d3f28195663958519ed0826f
Shie Erlich <erlich@users.sourceforge.net>
2003-08-27 23:03:38 +0000

    add: new command line

--- krusader/GUI/kcmdline.cpp
+++ krusader/GUI/kcmdline.cpp
@@ -1,32 +1,32 @@
 /***************************************************************************
-                                kcmdline.cpp
-                             -------------------
-    copyright            : (C) 2000 by Shie Erlich & Rafi Yanai
-    e-mail               : krusader@users.sourceforge.net
-    web site             : http://krusader.sourceforge.net
- ---------------------------------------------------------------------------
-  Description 
- ***************************************************************************
+                               kcmdline.cpp
+                            -------------------
+   copyright            : (C) 2000 by Shie Erlich & Rafi Yanai
+   e-mail               : krusader@users.sourceforge.net
+   web site             : http://krusader.sourceforge.net
+---------------------------------------------------------------------------
+ Description 
+***************************************************************************
 
-  A 
+ A 
 
-     db   dD d8888b. db    db .d8888.  .d8b.  d8888b. d88888b d8888b.
-     88 ,8P' 88  `8D 88    88 88'  YP d8' `8b 88  `8D 88'     88  `8D
-     88,8P   88oobY' 88    88 `8bo.   88ooo88 88   88 88ooooo 88oobY'
-     88`8b   88`8b   88    88   `Y8b. 88~~~88 88   88 88~~~~~ 88`8b
-     88 `88. 88 `88. 88b  d88 db   8D 88   88 88  .8D 88.     88 `88.
-     YP   YD 88   YD ~Y8888P' `8888Y' YP   YP Y8888D' Y88888P 88   YD
+    db   dD d8888b. db    db .d8888.  .d8b.  d8888b. d88888b d8888b.
+    88 ,8P' 88  `8D 88    88 88'  YP d8' `8b 88  `8D 88'     88  `8D
+    88,8P   88oobY' 88    88 `8bo.   88ooo88 88   88 88ooooo 88oobY'
+    88`8b   88`8b   88    88   `Y8b. 88~~~88 88   88 88~~~~~ 88`8b
+    88 `88. 88 `88. 88b  d88 db   8D 88   88 88  .8D 88.     88 `88.
+    YP   YD 88   YD ~Y8888P' `8888Y' YP   YP Y8888D' Y88888P 88   YD
 
-                                                     S o u r c e    F i l e
+                                                    S o u r c e    F i l e
 
- ***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
 
 
 #include "kcmdline.h"
@@ -49,145 +49,112 @@
 #include <kglobalsettings.h>
 #include <qfontmetrics.h>
 #include <qimage.h>
+#include <qstringlist.h>
+#include <qsizepolicy.h>
 
 
-KCMDLine::KCMDLine(QWidget *parent, const char *name ) : QWidget(parent,name) {
-  QGridLayout *layout=new QGridLayout(this,1,4);
-  path=new QLabel(this);
-  QWhatsThis::add( path, i18n( "Name of directory where command will be processed." ) );
-  path->setAlignment(Qt::AlignRight);
-  path->setFrameStyle(QFrame::Box | QFrame::Sunken);
-  path->setLineWidth(1);
-  path->setFont(KGlobalSettings::generalFont());
-  path->setMaximumHeight(QFontMetrics(path->font()).height()+4);
-  layout->addWidget(path,0,0);
+KCMDLine::KCMDLine( QWidget *parent, const char *name ) : QWidget( parent, name ) {
+  QGridLayout * layout = new QGridLayout( this, 1, 3 );
+  path = new QLabel( this );
+  QWhatsThis::add
+    ( path, i18n( "Name of directory where command will be processed." ) );
+  path->setAlignment( Qt::AlignRight );
+  path->setFrameStyle( QFrame::Box | QFrame::Sunken );
+  path->setLineWidth( 1 );
+  path->setFont( KGlobalSettings::generalFont() );
+  path->setMaximumHeight( QFontMetrics( path->font() ).height() + 4 );
+  path->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Preferred));
+  layout->addWidget( path, 0, 0 );
+
   // and editable command line
-  completion.setMode(KURLCompletion::FileCompletion);
-	cmdLine=new KRLineEdit(this);
-  cmdLine->setFont(KGlobalSettings::generalFont());
-  cmdLine->setMaximumHeight(QFontMetrics(cmdLine->font()).height()+4);
-  cmdLine->setCompletionObject(&completion);
-	QWhatsThis::add(cmdLine,i18n("<qt>Well, it's quite simple actually: You write "
-								 "you command in here, and Krusader obeys.<p>"
-								 "<Tip:>Move in command-line history with &lt;Up&gt; "
-								 "and &lt;Down&gt; arrows.</qt>" ) );
-  layout->addWidget(cmdLine,0,1);
-  // history button
-  history=new QToolButton(this);
-  history->setFixedSize(cmdLine->maximumHeight(),cmdLine->maximumHeight());
-  history->setTextLabel(i18n("Show the last commands you typed in the command line."));
-  QImage im = krLoader->loadIcon("date",KIcon::Panel).convertToImage();
-  history->setPixmap(im.scale(history->height(), history->height()));
-	historyMenu=new QPopupMenu(this);
-	historyMenu->setFont(KGlobalSettings::generalFont());
-  history->setPopup(historyMenu);	
-  history->setPopupDelay(1);
-	connect(historyMenu,SIGNAL(activated(int)),this,
-          SLOT(setCmdLineText(int)));	
-	QWhatsThis::add(history,i18n("The history button pops up a menu with the last 20 "
-								 "commands you wrote to the command line. Choose your path..."));
-  layout->addWidget(history,0,2);
+  completion.setMode( KURLCompletion::FileCompletion );
+  cmdLine = new KHistoryCombo( true, this );
+  cmdLine->setMaxCount(100);  // remember 100 commands
+  cmdLine->setFont( KGlobalSettings::generalFont() );
+  cmdLine->setMaximumHeight( QFontMetrics( cmdLine->font() ).height() + 4 );
+  cmdLine->setCompletionObject( &completion );
+  cmdLine->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred));
+  // load the history
+  KConfigGroupSaver grpSvr( krConfig, "Private" );
+  QStringList list = krConfig->readListEntry( "cmdline history" );
+  cmdLine->setHistoryItems( list );
+
+  connect( cmdLine, SIGNAL( activated( const QString& ) ), this, SLOT( slotRun( const QString& ) ) );
+  connect( cmdLine, SIGNAL( activated(const QString &) ), cmdLine, SLOT( clearEdit() ) );
+
+  QWhatsThis::add
+    ( cmdLine, i18n( "<qt>Well, it's quite simple actually: You write "
+                     "you command in here, and Krusader obeys.<p>"
+                     "<Tip:>Move in command-line history with &lt;Up&gt; "
+                     "and &lt;Down&gt; arrows.</qt>" ) );
+  layout->addWidget( cmdLine, 0, 1 );
+
   // a run in terminal button
-  terminal=new QToolButton(this);
-  terminal->setFixedSize(cmdLine->maximumHeight(),cmdLine->maximumHeight());
-  terminal->setTextLabel(i18n("If pressed, Krusader executes command line in a terminal."));
-  terminal->setToggleButton(true);
-  terminal->setOn(false);
-  im = krLoader->loadIcon("konsole",KIcon::Panel).convertToImage();
-  terminal->setPixmap(im.scale(terminal->height(), terminal->height()));
-  QWhatsThis::add(terminal,i18n("The 'run in terminal' button allows Krusader "
-			  					"to run console (or otherwise non-graphical) "
-								"programs in a terminal of your choice. If it's "
-								"pressed, terminal mode is active."));
-  layout->addWidget(terminal,0,3);
+  terminal = new QToolButton( this );
+  terminal->setFixedSize( cmdLine->maximumHeight(), cmdLine->maximumHeight() );
+  terminal->setTextLabel( i18n( "If pressed, Krusader executes command line in a terminal." ) );
+  terminal->setToggleButton( true );
+  terminal->setOn( false );
+  QImage im = krLoader->loadIcon( "konsole", KIcon::Panel ).convertToImage();
+  terminal->setPixmap( im.scale( terminal->height(), terminal->height() ) );
+  QWhatsThis::add
+    ( terminal, i18n( "The 'run in terminal' button allows Krusader "
+                      "to run console (or otherwise non-graphical) "
+                      "programs in a terminal of your choice. If it's "
+                      "pressed, terminal mode is active." ) );
+  layout->addWidget( terminal, 0, 2 );
   layout->activate();
   // connections
-  connect(cmdLine,SIGNAL(returnFocus()),this,SLOT(slotReturnFocus()));
-  connect(cmdLine,SIGNAL(returnPressed(const QString&)),this,
-          SLOT(slotRun(/*const QString&*/)));
-  connect(cmdLine,SIGNAL(upkeyPressed()),this,
-  				SLOT(slotHistoryUp()));
-  connect(cmdLine,SIGNAL(downkeyPressed()),this,
-  				SLOT(slotHistoryDown()));
-	historySize=0;
-	currentItem=-1;	// which means up key wasn't pressed yet
-	historyList.setAutoDelete(true);
+  connect( cmdLine, SIGNAL( returnFocus() ), this, SLOT( slotReturnFocus() ) );
 }
 
-void KCMDLine::setCurrent(const QString &p) {
-  path->setText(p+">");
-  completion.setDir(p);
+void KCMDLine::setCurrent( const QString &p ) {
+  path->setText( p + ">" );
+  completion.setDir( p );
 }
 
 
-KCMDLine::~KCMDLine(){
+KCMDLine::~KCMDLine() {
+   KConfigGroupSaver grpSvr( krConfig, "Private" );
+   QStringList list = cmdLine->historyItems();
+   kdWarning() << list[0] << endl;
+   krConfig->writeEntry( "cmdline history", list );
+   krConfig->sync();
 }
 
-void KCMDLine::slotRun(){
-  QString command1=cmdLine->text();
-  if(command1.isEmpty()) return;
-  krConfig->setGroup("General"); 
-  cmdLine->clear();
-	addToHistory(command1);
-	currentItem=-1;	// reset the up/down keys
-  QString panelPath=path->text().left( path->text().length()-1);
-  	
-	if (command1.left(3)=="cd ") { // cd command effect the active panel
-		QString dir = command1.right(command1.length()-command1.find(" ")).stripWhiteSpace();
-		if (dir == "~") dir = QDir::homeDirPath();
-		else
-		if (dir.left(1)!="/" && !dir.contains(":/")) dir=panelPath+(panelPath=="/" ? "" : "/")+dir;
-		SLOTS->refresh(dir);
-	} else {
-		QString save = getcwd(0,0);
-		KShellProcess proc;
-		chdir( panelPath.local8Bit() );
-		// run in a terminal ???
-		if (terminal->isOn()) proc << krConfig->readEntry("Terminal",_Terminal)
-		                           << "-e";
-		proc <<  command1;
-  	proc.start(KProcess::DontCare);
-  	
-		chdir(save.local8Bit());
-	}
+void KCMDLine::slotRun(const QString &command1) {
+  //cmdLine->clearEdit(); // clean the line edit
+
+  if ( command1.isEmpty() )
+    return ;
+  krConfig->setGroup( "General" );
+  QString panelPath = path->text().left( path->text().length() - 1 );
+
+  cmdLine->addToHistory(command1);
+
+  if ( command1.simplifyWhiteSpace().left( 3 ) == "cd " ) { // cd command effect the active panel
+    QString dir = command1.right( command1.length() - command1.find( " " ) ).stripWhiteSpace();
+    if ( dir == "~" )
+      dir = QDir::homeDirPath();
+    else
+      if ( dir.left( 1 ) != "/" && !dir.contains( ":/" ) )
+        dir = panelPath + ( panelPath == "/" ? "" : "/" ) + dir;
+    SLOTS->refresh( dir );
+  } else {
+    QString save = getcwd( 0, 0 );
+    KShellProcess proc;
+    chdir( panelPath.local8Bit() );
+    // run in a terminal ???
+    if ( terminal->isOn() )
+      proc << krConfig->readEntry( "Terminal", _Terminal )
+      << "-e";
+    proc << command1;
+    proc.start( KProcess::DontCare );
+
+    chdir( save.local8Bit() );
+  }
 }
 
-void KCMDLine::slotHistoryUp() {
-	if (currentItem==((signed)historyList.count())-1) return;	// top of the history list
-	cmdLine->setText(*historyList.at(++currentItem));
-}
-
-void KCMDLine::slotHistoryDown() {
-	if (currentItem<=0) return;	// top of the history list
-	cmdLine->setText(*historyList.at(--currentItem));
-}
-	
-// history remembers up to 20 commands
-  //////////////////////////////////////
-void KCMDLine::addToHistory(QString cmd) {
-	if (historyList.count()==20)
-		if (!historyList.removeLast()) {
-			kdFatal() << "KCmdLine: Unable to remove item from list. bailing out" << endl;
-			exit(0);
-		}
-	QString *temp=new QString(cmd);
-	if (!historyList.insert(0,temp)) {
-		kdFatal() << "KCmdLine: Unable to allocate memory. bailing out" << endl;
-		exit(0);
-	}
-	refreshHistory();
-}
-
-void KCMDLine::refreshHistory() {
-  historyMenu->clear();   // just in case
-	for (int i=historyList.count()-1; i>=0; --i)	
-	    historyMenu->insertItem(*historyList.at(i),i);
-}
-
-void KCMDLine::setCmdLineText(int id) {
-	cmdLine->setText(*historyList.at(id));
-	currentItem=id;
-}
 
 void KCMDLine::slotReturnFocus() {
   Krusader::App->mainView->cmdLineUnFocus();
--- krusader/GUI/kcmdline.h
+++ krusader/GUI/kcmdline.h
@@ -1,33 +1,33 @@
 /***************************************************************************
-                                kcmdline.h
-                             -------------------
-    begin                : Thu May 4 2000
-    copyright            : (C) 2000 by Shie Erlich & Rafi Yanai
-    e-mail               : krusader@users.sourceforge.net
-    web site             : http://krusader.sourceforge.net
- ---------------------------------------------------------------------------
-  Description
- ***************************************************************************
+                               kcmdline.h
+                            -------------------
+   begin                : Thu May 4 2000
+   copyright            : (C) 2000 by Shie Erlich & Rafi Yanai
+   e-mail               : krusader@users.sourceforge.net
+   web site             : http://krusader.sourceforge.net
+---------------------------------------------------------------------------
+ Description
+***************************************************************************
 
-  A
+ A
 
-     db   dD d8888b. db    db .d8888.  .d8b.  d8888b. d88888b d8888b.
-     88 ,8P' 88  `8D 88    88 88'  YP d8' `8b 88  `8D 88'     88  `8D
-     88,8P   88oobY' 88    88 `8bo.   88ooo88 88   88 88ooooo 88oobY'
-     88`8b   88`8b   88    88   `Y8b. 88~~~88 88   88 88~~~~~ 88`8b
-     88 `88. 88 `88. 88b  d88 db   8D 88   88 88  .8D 88.     88 `88.
-     YP   YD 88   YD ~Y8888P' `8888Y' YP   YP Y8888D' Y88888P 88   YD
+    db   dD d8888b. db    db .d8888.  .d8b.  d8888b. d88888b d8888b.
+    88 ,8P' 88  `8D 88    88 88'  YP d8' `8b 88  `8D 88'     88  `8D
+    88,8P   88oobY' 88    88 `8bo.   88ooo88 88   88 88ooooo 88oobY'
+    88`8b   88`8b   88    88   `Y8b. 88~~~88 88   88 88~~~~~ 88`8b
+    88 `88. 88 `88. 88b  d88 db   8D 88   88 88  .8D 88.     88 `88.
+    YP   YD 88   YD ~Y8888P' `8888Y' YP   YP Y8888D' Y88888P 88   YD
 
-                                                     H e a d e r    F i l e
+                                                    H e a d e r    F i l e
 
- ***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
 
 
 #ifndef KCMDLINE_H
@@ -38,76 +38,30 @@
 #include <qlabel.h>
 #include <qlayout.h>
 #include <qtoolbutton.h>
-#include <qlist.h>
-#include <qpopupmenu.h>
 
 // KDE includes
 #include <klineedit.h>
 #include <kshellcompletion.h>
+#include <kcombobox.h>
 
-// This class adds the UP/DOWN keys functionality to KLineEdit
-/////////////////////////////////////////////////////////////
-class KRLineEdit : public KLineEdit {
-	Q_OBJECT
-public:
-	KRLineEdit(QWidget *parent) : KLineEdit(parent,0) {
-	  setAcceptDrops(true);
-	  setCompletionMode(KGlobalSettings::CompletionAuto); // auto complete
-	}
+class KCMDLine : public QWidget {
+    Q_OBJECT
+  public:
+    KCMDLine( QWidget *parent = 0, const char *name = 0 );
+    ~KCMDLine();
+    void setCurrent( const QString & );
 
-signals:
-	void upkeyPressed();
-	void downkeyPressed();
-  void returnFocus();    // when user asks to return focus to panel
-		
-protected:
-	void keyPressEvent(QKeyEvent *e) {
-		switch (e->key()) {
-			case Key_Up :
-				if (e->state()==ControlButton)
-				  emit returnFocus(); // user asked to return focus to panel
-				   else emit upkeyPressed();
-				break;			
-			case Key_Down :
-			  emit downkeyPressed();
-			  break;
-			default:
-				KLineEdit::keyPressEvent(e);
-				break;
-		}	
-	}
-};
+  public slots:
+    inline void setFocus() { cmdLine->setFocus(); } // overloaded for KCmdLine
+    void slotReturnFocus(); // returns keyboard focus to panel
+    void slotRun(const QString &command1);
+    void addText( QString text ) { cmdLine->setCurrentText( cmdLine->currentText() + text ); }
 
-//class KRLineEdit;
-
-class KCMDLine : public QWidget  {
-   Q_OBJECT
-public: 
-	KCMDLine(QWidget *parent=0, const char *name=0);
-	~KCMDLine();
-	void setCurrent(const QString &);
-	
-public slots:
-  inline void setFocus() { cmdLine->setFocus(); } // overloaded for KCmdLine
-  void slotReturnFocus(); // returns keyboard focus to panel
-  void slotRun();
-  void slotHistoryUp();
-  void slotHistoryDown();
-  void setCmdLineText(int id);
-  void addText(QString text) { cmdLine->setText(cmdLine->text()+text); }
-
-private:
-	void addToHistory(QString cmd);
-  void refreshHistory();
-		
-private:
-  QLabel    			 *path;
-  KRLineEdit       *cmdLine;
-  QToolButton      *terminal,*history;
-  QPopupMenu			 *historyMenu;
-  QList<QString>    historyList;
-  int               historySize,currentItem;
-	KShellCompletion  completion;
+  private:
+    QLabel *path;
+    KHistoryCombo *cmdLine;
+    QToolButton *terminal;
+    KShellCompletion completion;
 };
 
 
