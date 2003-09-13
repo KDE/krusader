f0106f6d54fb2461dd134100eeecca819008f245
Shie Erlich <erlich@users.sourceforge.net>
2003-09-13 09:52:33 +0000

    fix: double-ftp crash, fixed via deep-copies

--- krusader/Panel/krdetailedviewitem.cpp
+++ krusader/Panel/krdetailedviewitem.cpp
@@ -45,7 +45,9 @@
 #include <kmimetype.h>
 
 KrDetailedViewItem::KrDetailedViewItem(KrDetailedView *parent, QListViewItem *after, vfile *vf):
-  QObject(parent), KListViewItem(parent, after), KrViewItem(), _vf(vf), _view(parent) {
+  QObject(parent), KListViewItem(parent, after), KrViewItem(), _view(parent) {
+  if (vf) _vf = new vfile(*vf);
+  else _vf = 0L;
   repaintItem();
 }
 
