fa85d53ae7d4aebaebcecbbacd763c98d5e8d4e7
Shie Erlich <erlich@users.sourceforge.net>
2004-01-03 21:37:55 +0000

    *** empty log message ***

---
+++ krusader/Panel/krcalcspacedialog.cpp
@@ -0,0 +1,165 @@
+/***************************************************************************
+                          krcalcspacedialog.cpp  -  description
+                             -------------------
+    begin                : Fri Jan 2 2004
+    copyright            : (C) 2004 by Shie Erlich & Rafi Yanai
+ e-mail               : krusader@users.sourceforge.net
+ web site             : http://krusader.sourceforge.net
+---------------------------------------------------------------------------
+Description
+***************************************************************************
+
+A
+
+  db   dD d8888b. db    db .d8888.  .d8b.  d8888b. d88888b d8888b.
+  88 ,8P' 88  `8D 88    88 88'  YP d8' `8b 88  `8D 88'     88  `8D
+  88,8P   88oobY' 88    88 `8bo.   88ooo88 88   88 88ooooo 88oobY'
+  88`8b   88`8b   88    88   `Y8b. 88~~~88 88   88 88~~~~~ 88`8b
+  88 `88. 88 `88. 88b  d88 db   8D 88   88 88  .8D 88.     88 `88.
+  YP   YD 88   YD ~Y8888P' `8888Y' YP   YP Y8888D' Y88888P 88   YD
+
+                                                  S o u r c e    F i l e
+
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+// Qt Includes
+#include <qtimer.h>
+#include <qlayout.h>
+#include <qlabel.h>
+// KDE Includes
+#include <klocale.h>
+#include <kcursor.h>
+// Krusader Includes
+#include "krcalcspacedialog.h"
+#include "../krusader.h"
+
+/* --=={ Patch by Heiner <h.eichmann@gmx.de> }==-- */
+KrCalcSpaceDialog::CalcThread::CalcThread(KrCalcSpaceDialog * parent, vfs * files, const QStringList & names)
+	: m_totalSize(0), m_totalFiles(0), m_totalDirs(0), m_names(names), m_files(files), m_parent(parent)
+        , m_threadInUse(true), m_stop(false) {}
+
+void KrCalcSpaceDialog::CalcThread::cleanUp(){
+	if (m_threadInUse || !finished())
+		m_synchronizeUsageAccess.unlock();
+	else{
+		m_synchronizeUsageAccess.unlock(); // prevents a resource leak
+		// otherwise: no one needs this instance any more: delete it
+		delete this;
+	}
+}
+
+void KrCalcSpaceDialog::CalcThread::deleteInstance(){
+	// synchronize to avoid race condition.
+	m_synchronizeUsageAccess.lock();
+	m_threadInUse = false;
+	cleanUp();
+}
+
+void KrCalcSpaceDialog::CalcThread::run(){
+	if ( !m_names.isEmpty() ) // if something to do: do the calculation
+	for ( QStringList::ConstIterator name = m_names.begin(); name != m_names.end(); ++name )
+		m_files->vfs_calcSpace( *name, &m_totalSize, &m_totalFiles, &m_totalDirs , & m_stop);
+	// synchronize to avoid race condition.
+	m_synchronizeUsageAccess.lock();
+	cleanUp(); // this does not need the instance any more
+}
+
+void KrCalcSpaceDialog::CalcThread::stop(){
+	// cancel was pressed
+	m_stop = true;
+}
+
+KrCalcSpaceDialog::KrCalcSpaceDialog(QWidget *parent, vfs * files, const QStringList & names, bool autoclose) :
+	KDialogBase(parent, "KrCalcSpaceDialog", true, "Calculate Occupied Space", Ok|Cancel),
+	m_autoClose(autoclose), m_canceled(false), m_timerCounter(0){
+	// the dialog: The Ok button is hidden until it is needed
+	showButtonOK(false);
+	m_thread = new CalcThread(this, files, names);
+	m_pollTimer = new QTimer(this);
+	QWidget * mainWidget = new QWidget( this );
+	setMainWidget(mainWidget);
+	QVBoxLayout *topLayout = new QVBoxLayout( mainWidget, 0, spacingHint() );
+
+	m_label = new QLabel( "", mainWidget, "caption" );
+	showResult(); // fill m_label with something usefull
+	topLayout->addWidget( m_label );
+	topLayout->addStretch(10);
+}
+
+void KrCalcSpaceDialog::calculationFinished(){
+	// close dialog if auto close is true
+	if (m_autoClose){
+		done(0);
+		return;
+	}
+	// otherwise hide cancel and show ok button
+	showButtonCancel(false);
+	showButtonOK(true);
+	showResult(); // and show final result
+}
+
+/* This timer has two jobs: it polls the thread if it is finished. Polling is
+  better here as it might finish while the dialog builds up. Secondly it refreshes
+  the displayed result.
+ */
+void KrCalcSpaceDialog::timer(){
+	// thread finished?
+	if (m_thread->finished()){
+		// close dialog or switch buttons
+		calculationFinished();
+		m_pollTimer->stop(); // stop the polling. No longer needed
+		return;
+	}
+
+	// Every 10 pollings (1 second) refresh the displayed result
+	if (++m_timerCounter > 10){
+		m_timerCounter = 0;
+		showResult();
+	}
+}
+
+void KrCalcSpaceDialog::showResult(){
+	if (!m_thread) return;
+	QString msg;
+	QString fileName = ( ( m_thread->getNames().count() == 1 ) ? ( i18n( "Name: " ) + m_thread->getNames().first() + "\n" ) : QString( "" ) );
+	msg = fileName + i18n( "Total occupied space: %1\nin %2 directories and %3 files" ).
+		arg( KIO::convertSize( m_thread->getTotalSize() ) ).arg( m_thread->getTotalDirs() ).arg( m_thread->getTotalFiles() );
+	m_label->setText(msg);
+}
+
+void KrCalcSpaceDialog::slotCancel(){
+	m_thread->stop(); // notify teh thread to stop
+	m_canceled = true; // set the cancel flag
+	KDialogBase::slotCancel(); // close the dialog
+}
+
+KrCalcSpaceDialog::~KrCalcSpaceDialog(){
+	CalcThread * tmp = m_thread;
+	m_thread = 0; // do not access the thread anymore or core dump if smoe piece of code wrongly does
+	tmp->deleteInstance(); // Notify the thread, that the dialog does not need anymore.
+}
+
+void KrCalcSpaceDialog::exec(){
+	m_thread->start(); // start the thread
+	if (m_autoClose){ // autoclose
+		// set the cursor to busy mode and wait 3 seconds or until the thread finishes
+		krApp->setCursor( KCursor::waitCursor() );
+		bool result = m_thread->wait(3000);
+		krApp->setCursor( KCursor::arrowCursor() );  // return the cursor to normal mode
+		if (result) return;// thread finished: do not show the dialog
+		showResult(); // fill the invisible dialog with usefull data
+	}
+	// prepare and start the poll timer
+	connect(m_pollTimer, SIGNAL(timeout()), this, SLOT(timer()));
+	m_pollTimer->start(100);
+	KDialogBase::exec(); // show the dialog
+}
+/* --=={ End of patch by Heiner <h.eichmann@gmx.de> }==-- */
+
---
+++ krusader/Panel/krcalcspacedialog.h
@@ -0,0 +1,101 @@
+/***************************************************************************
+                          krcalcspacedialog.h  -  description
+                             -------------------
+    begin                : Fri Jan 2 2004
+    copyright            : (C) 2004 by Shie Erlich & Rafi Yanai
+    e-mail               : krusader@users.sourceforge.net
+    web site             : http://krusader.sourceforge.net
+ ---------------------------------------------------------------------------
+  Description
+ ***************************************************************************
+
+  A
+
+     db   dD d8888b. db    db .d8888.  .d8b.  d8888b. d88888b d8888b.
+     88 ,8P' 88  `8D 88    88 88'  YP d8' `8b 88  `8D 88'     88  `8D
+     88,8P   88oobY' 88    88 `8bo.   88ooo88 88   88 88ooooo 88oobY'
+     88`8b   88`8b   88    88   `Y8b. 88~~~88 88   88 88~~~~~ 88`8b
+     88 `88. 88 `88. 88b  d88 db   8D 88   88 88  .8D 88.     88 `88.
+     YP   YD 88   YD ~Y8888P' `8888Y' YP   YP Y8888D' Y88888P 88   YD
+
+                                                     H e a d e r    F i l e
+
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KRCALCSPACEDIALOG_H
+#define KRCALCSPACEDIALOG_H
+
+/* --=={ Patch by Heiner <h.eichmann@gmx.de> }==-- */
+
+// KDE Includes
+#include <kdialogbase.h>
+#include <kio/jobclasses.h>
+// Qt Includes
+#include <qthread.h>
+// Krusader Includes
+#include "../VFS/vfs.h"
+
+
+/* Dialog calculating showing the number of files and directories and its total size
+   in a dialog. If wanted, the dialog appears after 3 seconds of calculation, to
+   avoid a short appearence if the result was found quickly. Computes teh result in
+   a different thread.
+ */
+class KrCalcSpaceDialog : public KDialogBase{
+	Q_OBJECT
+	/* Thread which does the actual calculation. Deletes itself, if no longer
+	   needed. Creator must call finished(), if the thread is no longer needed.
+	*/
+	class CalcThread : public QThread{
+		KIO::filesize_t m_totalSize;
+		unsigned long m_totalFiles;
+		unsigned long m_totalDirs;
+		const QStringList m_names;
+		vfs * m_files;
+		KrCalcSpaceDialog * m_parent;
+		QMutex m_synchronizeUsageAccess;
+		bool m_threadInUse; // true: caller needs the thread
+		bool m_stop;
+		void cleanUp(); // Deletes this, if possible
+	public:
+		KIO::filesize_t getTotalSize() const {return m_totalSize;} // the result
+		unsigned long getTotalFiles() const {return m_totalFiles;} // the result
+		unsigned long getTotalDirs() const {return m_totalDirs;} // the result
+		const QStringList & getNames() const {return m_names;} // list of directories to calculate
+		CalcThread(KrCalcSpaceDialog * parent, vfs * files, const QStringList & names);
+		void deleteInstance(); // thread is no longer needed.
+		void run(); // start calculation
+		void stop(); // stop it. Thread continues until vfs_calcSpace returns
+	} * m_thread;
+	friend class CalcThread;
+	class QTimer * m_pollTimer;
+	QLabel * m_label;
+	bool m_autoClose; // true: wait 3 sec. before showing the dialog. Close it, when done
+	bool m_canceled; // true: cancel was pressed
+	int m_timerCounter; // internal counter. The timer runs faster as the rehresh (see comment there)
+	void calculationFinished(); // called if the calulation is done
+	void showResult(); // show the current result in teh dialog
+protected slots:
+	void timer(); // poll timer was fired
+	void slotCancel(); // cancel was pressed
+public:
+	// autoclose: wait 3 sec. before showing the dialog. Close it, when done
+	KrCalcSpaceDialog(QWidget *parent, vfs * files, const QStringList & names, bool autoclose);
+	~KrCalcSpaceDialog();
+	KIO::filesize_t getTotalSize() const {return m_thread->getTotalSize();} // the result
+	unsigned long getTotalFiles() const {return m_thread->getTotalFiles();} // the result
+	unsigned long getTotalDirs() const {return m_thread->getTotalDirs();} // the result
+	bool wasCanceled() const {return m_canceled;} // cancel was pressed; result is probably wrong
+public slots:
+	void exec(); // start calculation
+};
+/* End of patch by Heiner <h.eichmann@gmx.de> */
+
+#endif
