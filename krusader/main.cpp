2f2a538f213332daf392b026e4cfe2bb6e7a200f
Shie Erlich <erlich@users.sourceforge.net>
2004-03-02 20:49:42 +0000

    add: hans name in the 'thanks' section

--- krusader/main.cpp
+++ krusader/main.cpp
@@ -66,6 +66,7 @@ int main(int argc, char *argv[]) {
   aboutData.addAuthor("Frank Schoolmeesters", "Documentation Manager", "frank_schoolmeesters@fastmail.fm", 0);
   aboutData.addCredit("Heiner Eichmann","The 6th Beatle: FreeBSD port, patches and general help", "h.eichmann@gmx.de", 0);
   aboutData.addCredit("Jan Halasa", "Bookmark Module", "xhalasa@fi.muni.cz", 0);
+  aboutData.addCredit("Hans Loeffler", "Dir history button", 0, 0);
   aboutData.addCredit("Mikolaj Machowski", "Usability and QA", "<mikmach@wp.pl>", 0);
   aboutData.addCredit("Cristi Dumitrescu","QA, bug-hunting, patches and general help","cristid@chip.ro",0);
   aboutData.addCredit("Aurelien Gateau","patch for KViewer","aurelien.gateau@free.fr",0);
