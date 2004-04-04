449cb2f436ca00ccf8f9dbae952a17fb65334579
Csaba Karai <cskarai@freemail.hu>
2004-04-04 22:05:30 +0000

    FIXED: df maximum waiting time is 60s

--- krusader/MountMan/kmountman.cpp
+++ krusader/MountMan/kmountman.cpp
@@ -63,7 +63,7 @@ YP   YD 88   YD ~Y8888P' `8888Y' YP   YP Y8888D' Y88888P 88   YD
 #define FSTAB "/etc/fstab"
 #endif
 
-#define  DF_WAIT_TIME             30
+#define  DF_WAIT_TIME             60
 
 bool dfStartFailed = false;
 
