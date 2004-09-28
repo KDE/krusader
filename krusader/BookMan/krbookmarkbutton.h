bc5ff119e2fc041fb7243af61763c853785a40ce
Shie Erlich <erlich@users.sourceforge.net>
2004-09-28 23:15:33 +0000

    bookman2 opens urls ;-)

--- krusader/BookMan/krbookmarkbutton.h
+++ krusader/BookMan/krbookmarkbutton.h
@@ -2,10 +2,15 @@
 #define KRBOOKMARK_BUTTON_H
 
 #include <qtoolbutton.h>
+#include "krbookmarkhandler.h"
 
 class KrBookmarkButton: public QToolButton {
 public:
 	KrBookmarkButton(QWidget *parent);
+	KrBookmarkHandler *handler() const { return _handler; }
+	
+private:
+	KrBookmarkHandler *_handler;
 };
 
 #endif // KRBOOKMARK_BUTTON_H
