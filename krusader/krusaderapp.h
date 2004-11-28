0d1b89f0f895c65c6562406a4e3d895f14dff911
Shie Erlich <erlich@users.sourceforge.net>
2004-11-28 22:43:33 +0000

    *** empty log message ***

---
+++ krusader/krusaderapp.h
@@ -0,0 +1,31 @@
+#ifndef KRUSADERAPP_H
+#define KRUSADERAPP_H
+
+#include <kapplication.h>
+#include "X11/Xlib.h"
+
+// declare a dummy kapplication, just to get the X focusin focusout events
+class KrusaderApp: public KApplication {
+	Q_OBJECT
+public:
+	KrusaderApp(): KApplication() {}
+	bool x11EventFilter ( XEvent *e ) {
+		switch (e->type) {
+			case FocusIn:
+				emit windowActive();
+				break;
+			case FocusOut:
+				emit windowInactive();
+				break;
+			
+		}
+		return false; // event should be processed normally
+	}
+
+signals:
+	void windowActive();
+	void windowInactive();
+};
+
+
+#endif // KRUSADERAPP_H
