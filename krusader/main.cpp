5c35e51bdcfd6eebc88073eb3a83671a29747c20
Frank Schoolmeesters <codeknight@users.sourceforge.net>
2005-04-15 11:13:43 +0000

    "fixed" Jan Willem van de Meent (Adios) name ;)

--- krusader/main.cpp
+++ krusader/main.cpp
@@ -75,7 +75,7 @@ int main(int argc, char *argv[]) {
   aboutData.addCredit("Jan Halasa", "The old Bookmark Module", "xhalasa@fi.muni.cz", 0);
   aboutData.addCredit("Hans Loeffler", "Dir history button", 0, 0);
   aboutData.addCredit("Szombathelyi György", "ISO KIO slave", 0, 0);
-  aboutData.addCredit("Jan Willem", "Icons for Krusader", 0, 0);
+  aboutData.addCredit("Jan Willem van de Meent (Adios)", "Icons for Krusader", 0, 0);
   aboutData.addCredit("Mikolaj Machowski", "Usability and QA", "<mikmach@wp.pl>", 0);
   aboutData.addCredit("Cristi Dumitrescu","QA, bug-hunting, patches and general help","cristid@chip.ro",0);
   aboutData.addCredit("Aurelien Gateau","patch for KViewer","aurelien.gateau@free.fr",0);
