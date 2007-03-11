8d6758aff4c386d2328c32eaf6701b3a2f571acd
Jonas Bähr <jonas.baehr@web.de>
2007-03-11 23:14:07 +0000

    CHANGED: Useractions: much better handling of the Each-placeholder patch by Jiri Palacek, thanks! See CVSNEWS for details

---
+++ krusader/UserAction/kractionbase.cpp
@@ -0,0 +1,77 @@
+//
+// C++ Implementation: kractionbase
+//
+// Description: 
+//
+//
+// Author: Shie Erlich and Rafi Yanai <>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include <kinputdialog.h>
+#include <klocale.h>
+
+#include <qerrormessage.h>
+
+#include "kraction.h"
+#include "expander.h"
+#include "kractionbase.h"
+
+KrActionBase::~KrActionBase()
+{
+}
+
+void KrActionBase::exec() {
+	KrActionProc *proc;
+
+   // replace %% and prepare string
+	QStringList commandList;
+	if(doSubstitution()) {
+		Expander exp;
+		exp.expand(command(),acceptURLs());
+		if(exp.error()) {
+			handleError(exp.error());
+			return;
+		}
+		commandList=exp.result();
+	} else
+		commandList=command();
+   //TODO: query expander for status and may skip the rest of the function
+
+   // stop here if the commandline is empty
+   if ( commandList.count() == 1 && commandList[0].stripWhiteSpace().isEmpty() )
+       return;
+
+   if ( confirmExecution() ) {
+       for ( QStringList::iterator it = commandList.begin(); it != commandList.end(); ++it ) {
+           bool exec = true;
+           *it = KInputDialog::getText(
+        i18n( "Confirm execution" ),
+        i18n( "Command being executed:" ),
+        *it,
+        &exec, 0
+      );
+      if ( exec ) {
+        proc = actionProcFactoryMethod();
+        proc->start( *it );
+      }
+    } //for
+  } // if ( _properties->confirmExecution() )
+  else {
+    proc = actionProcFactoryMethod();
+    proc->start( commandList );
+  }
+
+}
+
+void KrActionBase::handleError(const Error& err)
+{
+	QErrorMessage::qtHandler()->message(err.what());
+}
+
+KrActionProc* KrActionBase::actionProcFactoryMethod()
+{
+	return new KrActionProc(this);
+}
---
+++ krusader/UserAction/kractionbase.h
@@ -0,0 +1,97 @@
+//
+// C++ Interface: kractionbase
+//
+// Description: 
+//
+//
+// Author: Shie Erlich and Rafi Yanai <>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef KRACTIONBASE_H
+#define KRACTIONBASE_H
+
+#include <qstring.h>
+#include "expander.h"
+class KrActionProc;
+
+class KrActionBase
+{
+public:
+  KrActionBase() {}
+  virtual ~KrActionBase();
+
+	/** \brief Specifies the mode for executing the action */
+  enum ExecType { 
+		Normal, ///< Run the command freely
+		Terminal, ///< Run the command in new terminal window
+		CollectOutput, ///< Collect output from this command
+		CollectOutputSeparateStderr, ///< Like #CollectOutput, but display stderr output separately
+		RunInTE ///< Run in built-in terminal emulator
+	};
+
+	/** \brief Command which runs this action
+	 * 
+	 * The string of the command may contain placeholders
+	 * which are parsed by the #Expander class, unless #doSubstitution 
+	 * returns false
+	 * 
+	 * The command is run by the shell, which should be bash (see #Expander)
+	 * 
+	 * @see Expander
+	 * @see doSubstitution
+	 * 
+	 * @return The command to execute
+	 */
+  virtual QString command() const =0;
+	/** \brief Execution type of the action
+	 * 
+	 * @see #ExecType
+	 */
+  virtual ExecType execType() const =0;
+	/** \brief Working directory of the command
+	 * 
+	 * @return The working directory of the command. May be \a null,
+	 *   in which case the command is executed in current directory
+	 */
+  virtual QString startpath() const =0;
+	/** \brief Username under which the command is run
+	 * 
+	 * @return The username of the command. May be \a null,
+	 *   in which case the command is executed under the current user
+	 */
+  virtual QString user() const=0;
+	/** \brief Name of the action
+	 * 
+	 * @return The name of the action which will be shown to the user
+	 *   eg. any string will do
+	 */
+  virtual QString text() const=0;
+	/** \brief Does the command accept URLs as filenames (like KDE apps)?
+	 * 
+	 * @return \a true iff it does
+	 */
+  virtual bool acceptURLs() const=0;
+	/** \brief Confirm execution of this action by the user?
+	 * 
+	 * @return \a true iff execution should be confirmed
+	 */
+  virtual bool confirmExecution() const=0;
+	/** \brief Can #command contain placeholders?
+	 * 
+	 * @return \a true iff #command should be expanded by #Expander
+	 */
+  virtual bool doSubstitution() const=0;
+	/** \brief A factory method for creating KrActionProc
+	 * 
+	 * @return A new instance of KrActionProc
+	 */
+	virtual KrActionProc* actionProcFactoryMethod();
+	virtual void handleError(const Error& err);
+
+  void exec();
+
+};
+
+#endif
