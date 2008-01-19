84e4dfa0a3c6b445d47c91f983464efb6eda83a7
Dirk Eschler <eschler@gmail.com>
2008-01-19 11:05:13 +0000

    year++

--- krusader/main.cpp
+++ krusader/main.cpp
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
 
 KAboutData aboutData( "krusader", 0, ( geteuid() ? ki18n("Krusader") : ki18n("Krusader - ROOT PRIVILEGES")),
     versionName.toLocal8Bit(), ki18n(description), KAboutData::License_GPL_V2,
-    ki18n("(c) 2000-2003, Shie Erlich, Rafi Yanai\n(c) 2004-2007, Krusader Krew"), 
+    ki18n("(c) 2000-2003, Shie Erlich, Rafi Yanai\n(c) 2004-2008, Krusader Krew"), 
     ki18n("Feedback:\nhttp://www.krusader.org/phpBB/\n\nIRC\nserver: irc.freenode.net, channel: #krusader"),
     "http://www.krusader.org");
 
