bb817792176309e4ca8f0d56bcbfcbe63543a2ab
Jonas Bähr <jonas.baehr@web.de>
2008-05-08 18:52:47 +0000

    Adapted createdist.sh to work with git and Mac OS X
    If the ''svn status'' command fails, I try ''git ls-files''. If after that
    the FILES variable is still empty, the script exits with an error message.
    
    Since git returns only files (not dirs), I've merged the directory creation
    process directly into the file copy loop. Then I changed the ''--preserve=all''
    option of cp to the posix compilant ''-p'' option; Mac OS X does not ship the
    GNU version of cp.

--- createdist.sh
+++ createdist.sh
@@ -8,25 +8,30 @@ rm -rf __KrDist__
 mkdir __KrDist__
 mkdir __KrDist__/$DIST_NAME
 
-echo 'SVN listing...'
-svn status -v >__KrDist__/svnfiles
-FILES=`cat __KrDist__/svnfiles | grep -v '^\? ' | awk '{print $NF;}'`
+echo 'try SVN listing...'
+if svn status -v >__KrDist__/svnfiles; then
+  FILES=`cat __KrDist__/svnfiles | grep -v '^\? ' | awk '{print $NF;}'`
+fi
 
-echo 'Creating directory structure...'
-for FILE in $FILES;
-do
-   if [ $FILE != "." ]; then
-     if [ -d $FILE ]; then
-        mkdir __KrDist__/$DIST_NAME/$FILE;
-     fi
-   fi
-done
+if [ -z "$FILES" ]; then
+  echo 'try git listing...'
+  if git ls-files >__KrDist__/gitfiles; then
+    FILES=`cat __KrDist__/gitfiles`
+  fi
+fi
+
+if [ -z "$FILES" ]; then
+  echo "Something whent wrong! Can't find version controlled source files"
+  exit 1
+fi
 
 echo 'Copying files for disting...'
 for FILE2 in $FILES;
 do
    if [ ! -d $FILE2 ]; then
-     cp --preserve=all $FILE2 __KrDist__/$DIST_NAME/$FILE2;
+     target_dir="__KrDist__/$DIST_NAME/`dirname $FILE2`"
+     if [ ! -d $target_dir ]; then mkdir -p $target_dir; fi
+     cp -p $FILE2 $target_dir;
    fi
 done
 
