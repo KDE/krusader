b4fa105cd26161a01e8a199d0482e095c4e6189c
Václav Jůza <vaclavjuza@gmail.com>
2009-06-02 22:34:51 +0000

    Don't forget to xgettext rc.cpp

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dirs="krusader krArc tar iso"
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find $dirs -name \*.cpp -o -name \*.cc -o -name \*.c -o -name \*.h` -o $podir/krusader.pot
+$XGETTEXT rc.cpp `find $dirs -name \*.cpp -o -name \*.cc -o -name \*.c -o -name \*.h` -o $podir/krusader.pot
 rm -f rc.cpp
