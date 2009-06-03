b3288e3ef7317b8667265a10a88143e6601baa47
Fathi Boudra <fabo@kde.org>
2009-06-03 15:48:42 +0000

    SVN_SILENT Krazy: copyright fixes

--- krusader/KViewer/krviewer.cpp
+++ krusader/KViewer/krviewer.cpp
@@ -1,26 +1,28 @@
-/***************************************************************************
-                         krviewer.cpp  -  description
-                            -------------------
-   begin                : Thu Apr 18 2002
-   copyright            : (C) 2002 by Shie Erlich & Rafi Yanai
-   email                :
-***************************************************************************/
+/*****************************************************************************
+ * Copyright (C) 2002 Shie Erlich <erlich@users.sourceforge.net>             *
+ * Copyright (C) 2002 Rafi Yanai <yanai@users.sourceforge.net>               *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This package is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this package; if not, write to the Free Software               *
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA *
+ *****************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/ 
-// Qt includes
 #include <qdatastream.h>
 #include <QtCore/QFile>
 #include <QtCore/QTimer>
 #include <QKeyEvent>
 #include <QEvent>
-// KDE includes
+
 #include <kmenubar.h>
 #include <kmimetype.h>
 #include <klocale.h>
@@ -39,7 +41,7 @@
 #include <kprocess.h>
 #include <kfileitem.h> 
 #include <ktoolbar.h>
-// Krusader includes
+
 #include "krviewer.h"
 #include "../krusader.h"
 #include "../defaults.h"
@@ -50,7 +52,6 @@
 #define VIEW_ICON     "zoom-original"
 #define EDIT_ICON     "edit"
 #define MODIFIED_ICON "document-save-as"
-
 
 QList<KrViewer *> KrViewer::viewers;
 
--- krusader/krservices.h
+++ krusader/krservices.h
@@ -1,19 +1,21 @@
-/***************************************************************************
-                          krservices.h  -  description
-                             -------------------
-    begin                : Thu Aug 8 2002
-    copyright            : (C) 2002 by Shie Erlich & Rafi Yanai
-    email                : 
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/*****************************************************************************
+ * Copyright (C) 2002 Shie Erlich <erlich@users.sourceforge.net>             *
+ * Copyright (C) 2002 Rafi Yanai <yanai@users.sourceforge.net>               *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This package is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this package; if not, write to the Free Software               *
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA *
+ *****************************************************************************/
 
 #ifndef KRSERVICES_H
 #define KRSERVICES_H
@@ -25,10 +27,6 @@
 
 class QTextStream;
 class QFile;
-
-/**
-  *@author Shie Erlich & Rafi Yanai
-  */
 
 class KrServices {
 public: 
@@ -55,7 +53,6 @@ private:
 	static QMap<QString,QString>* slaveMap;
 
 };
-
 
 // TODO: make KrServices a namespace and move it there
 
