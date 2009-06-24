0342d31be59e8409c337a7db59e2a26437bffd93
Fathi Boudra <fabo@kde.org>
2009-06-24 23:47:30 +0000

    Add myself to copyright header

--- krusader/Dialogs/newftpgui.h
+++ krusader/Dialogs/newftpgui.h
@@ -1,6 +1,7 @@
 /*****************************************************************************
  * Copyright (C) 2002 Shie Erlich <erlich@users.sourceforge.net>             *
  * Copyright (C) 2002 Rafi Yanai <yanai@users.sourceforge.net>               *
+ * Copyright (C) 2009 Fathi Boudra <fboudra@gmail.com>                       *
  *                                                                           *
  * This program is free software; you can redistribute it and/or modify      *
  * it under the terms of the GNU General Public License as published by      *
