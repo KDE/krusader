f9b92ed9e10527e0a9972d476f8911e8d1801e80
Jan Lepper <dehtris@yahoo.de>
2010-02-28 23:29:14 +0000

    new class KrInterView - base class for interview based views

---
+++ krusader/Panel/krinterview.cpp
@@ -0,0 +1,266 @@
+/*****************************************************************************
+ * Copyright (C) 2002 Shie Erlich <erlich@users.sourceforge.net>             *
+ * Copyright (C) 2002 Rafi Yanai <yanai@users.sourceforge.net>               *
+ * Copyright (C) 2010 Jan Lepper <dehtris@yahoo.de>                          *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This package is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this package; if not, write to the Free Software               *
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA *
+ *****************************************************************************/
+
+#include "krinterview.h"
+#include "krvfsmodel.h"
+#include "krinterviewitem.h"
+#include "krcolorcache.h"
+#include "krmousehandler.h"
+
+KrInterView::KrInterView(KConfig *cfg, QAbstractItemView *itemView) : KrView(cfg), _itemView(itemView), _mouseHandler(0)
+{
+     _model = new KrVfsModel(this);
+    _model->sort(KrVfsModel::Name, Qt::AscendingOrder);
+
+    // fix the context menu problem
+    int j = QFontMetrics(_itemView->font()).height() * 2;
+    _mouseHandler = new KrMouseHandler(this, j);
+}
+
+KrInterView::~KrInterView()
+{
+    delete _model;
+    _model = 0;
+    delete _mouseHandler;
+    _mouseHandler = 0;
+    QHashIterator< vfile *, KrInterViewItem *> it(_itemHash);
+    while (it.hasNext())
+        delete it.next().value();
+    _itemHash.clear();
+}
+
+KrViewItem* KrInterView::findItemByName(const QString &name)
+{
+    if (!_model->ready())
+        return 0;
+
+    QModelIndex ndx = _model->nameIndex(name);
+    if (!ndx.isValid())
+        return 0;
+    return getKrInterViewItem(ndx);
+}
+
+QString KrInterView::getCurrentItem() const
+{
+    if (!_model->ready())
+        return QString();
+
+    vfile *vf = _model->vfileAt(_itemView->currentIndex());
+    if (vf == 0)
+        return QString();
+    return vf->vfile_getName();
+}
+
+KrViewItem* KrInterView::getCurrentKrViewItem()
+{
+    if (!_model->ready())
+        return 0;
+
+    return getKrInterViewItem(_itemView->currentIndex());
+}
+
+KrViewItem* KrInterView::getFirst()
+{
+    if (!_model->ready())
+        return 0;
+
+    return getKrInterViewItem(_model->index(0, 0, QModelIndex()));
+}
+
+KrViewItem* KrInterView::getLast()
+{
+    if (!_model->ready())
+        return 0;
+
+    return getKrInterViewItem(_model->index(_model->rowCount() - 1, 0, QModelIndex()));
+}
+
+KrViewItem* KrInterView::getNext(KrViewItem *current)
+{
+    vfile* vf = (vfile *)current->getVfile();
+    QModelIndex ndx = _model->vfileIndex(vf);
+    if (ndx.row() >= _model->rowCount() - 1)
+        return 0;
+    return getKrInterViewItem(_model->index(ndx.row() + 1, 0, QModelIndex()));
+}
+
+KrViewItem* KrInterView::getPrev(KrViewItem *current)
+{
+    vfile* vf = (vfile *)current->getVfile();
+    QModelIndex ndx = _model->vfileIndex(vf);
+    if (ndx.row() <= 0)
+        return 0;
+    return getKrInterViewItem(_model->index(ndx.row() - 1, 0, QModelIndex()));
+}
+
+KrViewItem* KrInterView::getKrViewItemAt(const QPoint &vp)
+{
+    if (!_model->ready())
+        return 0;
+
+    return getKrInterViewItem(_itemView->indexAt(vp));
+}
+
+KrInterViewItem * KrInterView::getKrInterViewItem(const QModelIndex & ndx)
+{
+    if (!ndx.isValid())
+        return 0;
+    vfile * vf = _model->vfileAt(ndx);
+    if (vf == 0)
+        return 0;
+    QHash<vfile *, KrInterViewItem*>::iterator it = _itemHash.find(vf);
+    if (it == _itemHash.end()) {
+        KrInterViewItem * newItem =  new KrInterViewItem(this, vf);
+        _itemHash[ vf ] = newItem;
+        _dict.insert(vf->vfile_getName(), newItem);
+        return newItem;
+    }
+    return *it;
+}
+
+void KrInterView::makeCurrentVisible()
+{
+    _itemView->scrollTo(_itemView->currentIndex());
+}
+
+void KrInterView::makeItemVisible(const KrViewItem *item)
+{
+    if (item == 0)
+        return;
+    vfile* vf = (vfile *)item->getVfile();
+    QModelIndex ndx = _model->vfileIndex(vf);
+    if (ndx.isValid())
+        _itemView->scrollTo(ndx);
+}
+
+void KrInterView::setCurrentItem(const QString& name)
+{
+    QModelIndex ndx = _model->nameIndex(name);
+    if (ndx.isValid())
+        _itemView->setCurrentIndex(ndx);
+}
+
+void KrInterView::setCurrentKrViewItem(KrViewItem *item)
+{
+    if (item == 0) {
+        _itemView->setCurrentIndex(QModelIndex());
+        return;
+    }
+    vfile* vf = (vfile *)item->getVfile();
+    QModelIndex ndx = _model->vfileIndex(vf);
+    if (ndx.isValid() && ndx.row() != _itemView->currentIndex().row()) {
+        _mouseHandler->cancelTwoClickRename();
+        _itemView->setCurrentIndex(ndx);
+    }
+}
+
+void KrInterView::sort()
+{
+    _model->sort();
+}
+
+void KrInterView::clear()
+{
+    _itemView->clearSelection();
+    _model->clear();
+    QHashIterator< vfile *, KrInterViewItem *> it(_itemHash);
+    while (it.hasNext())
+        delete it.next().value();
+    _itemHash.clear();
+    KrView::clear();
+}
+
+void KrInterView::addItems(vfs* v, bool addUpDir)
+{
+    _model->setVfs(v, addUpDir);
+    _count = _model->rowCount();
+    if (addUpDir)
+        _count--;
+    _itemView->setCurrentIndex(_model->index(0, 0));
+    if (!nameToMakeCurrent().isEmpty())
+        setCurrentItem(nameToMakeCurrent());
+}
+
+KrViewItem* KrInterView::preAddItem(vfile *vf)
+{
+    return getKrInterViewItem(_model->addItem(vf));
+}
+
+bool KrInterView::preDelItem(KrViewItem *item)
+{
+    if (item == 0)
+        return true;
+    QModelIndex ndx = _model->removeItem((vfile *)item->getVfile());
+    if (ndx.isValid())
+        _itemView->setCurrentIndex(ndx);
+    _itemHash.remove((vfile *)item->getVfile());
+    return true;
+}
+
+void KrInterView::updateItem(vfile * item)
+{
+    if (item == 0)
+        return;
+    _model->updateItem(item);
+    op()->emitSelectionChanged();
+}
+
+void KrInterView::updateItem(KrViewItem* item)
+{
+    if (item == 0)
+        return;
+    updateItem((vfile *)item->getVfile());
+}
+
+void KrInterView::prepareForActive()
+{
+    KrView::prepareForActive();
+    _itemView->setFocus();
+    KrViewItem * current = getCurrentKrViewItem();
+    if (current != 0) {
+        QString desc = current->description();
+        op()->emitItemDescription(desc);
+    }
+}
+
+void KrInterView::prepareForPassive()
+{
+    KrView::prepareForPassive();
+    _mouseHandler->cancelTwoClickRename();
+    //if ( renameLineEdit() ->isVisible() )
+    //renameLineEdit() ->clearFocus();
+}
+
+void KrInterView::redraw()
+{
+    _itemView->viewport()->update();
+}
+
+void KrInterView::refreshColors()
+{
+    QPalette p(_itemView->palette());
+    KrColorGroup cg;
+    KrColorCache::getColorCache().getColors(cg, KrColorItemType(KrColorItemType::File,
+        false, _focused, false, false));
+    p.setColor(QPalette::Text, cg.text());
+    p.setColor(QPalette::Base, cg.background());
+    _itemView->setPalette(p);
+    _itemView->viewport()->update();
+}
---
+++ krusader/Panel/krinterview.h
@@ -0,0 +1,79 @@
+/*****************************************************************************
+ * Copyright (C) 2002 Shie Erlich <erlich@users.sourceforge.net>             *
+ * Copyright (C) 2002 Rafi Yanai <yanai@users.sourceforge.net>               *
+ * Copyright (C) 2010 Jan Lepper <dehtris@yahoo.de>                          *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This package is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this package; if not, write to the Free Software               *
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA *
+ *****************************************************************************/
+
+#ifndef KRINTERVIEW_H
+#define KRINTERVIEW_H
+
+#include <QVector>
+#include <QAbstractItemView>
+
+#include "krview.h"
+#include "krviewitem.h"
+
+class KrInterViewItem;
+class KrVfsModel;
+class KrMouseHandler;
+
+class KrInterView : public KrView
+{
+    friend class KrInterViewItem;
+public:
+    KrInterView(KConfig *cfg, QAbstractItemView *itemView);
+    ~KrInterView();
+    virtual QModelIndex getCurrentIndex() {
+        return _itemView->currentIndex();
+    }
+    virtual bool isSelected(const QModelIndex &ndx) {
+        return _itemView->selectionModel()->isSelected(ndx);
+    }
+    virtual KrViewItem* getFirst();
+    virtual KrViewItem* getLast();
+    virtual KrViewItem* getNext(KrViewItem *current);
+    virtual KrViewItem* getPrev(KrViewItem *current);
+    virtual KrViewItem* getCurrentKrViewItem();
+    virtual KrViewItem* findItemByName(const QString &name);
+    virtual void addItems(vfs* v, bool addUpDir = true);
+    virtual QString getCurrentItem() const;
+    virtual KrViewItem* getKrViewItemAt(const QPoint &vp);
+    virtual void setCurrentItem(const QString& name);
+    virtual void setCurrentKrViewItem(KrViewItem *item);
+    virtual void makeItemVisible(const KrViewItem *item);
+    virtual void clear();
+    virtual void updateItem(KrViewItem* item);
+    virtual void updateItem(vfile* item);
+    virtual void sort();
+    virtual void refreshColors();
+    virtual void redraw();
+    virtual void prepareForActive();
+    virtual void prepareForPassive();
+
+protected:
+    virtual KrViewItem* preAddItem(vfile *vf);
+    virtual bool preDelItem(KrViewItem *item);
+    KrInterViewItem * getKrInterViewItem(const QModelIndex &);
+    void makeCurrentVisible();
+
+    KrVfsModel *_model;
+    QAbstractItemView *_itemView;
+    KrMouseHandler *_mouseHandler;
+    QHash<vfile *, KrInterViewItem*> _itemHash;
+};
+
+#endif
