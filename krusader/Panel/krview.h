a05cf850357007607ea190fe791c410b67189e01
Jan Lepper <dehtris@yahoo.de>
2010-10-23 20:21:28 +0000

    added: KrView::enableUpdateDefaultSettings(bool)

--- krusader/Panel/krview.h
+++ krusader/Panel/krview.h
@@ -437,6 +437,9 @@ public:
     void changeSelection(const KRQuery& filter, bool select);
     void changeSelection(const KRQuery& filter, bool select, bool includeDirs);
     bool isFiltered(vfile *vf);
+    void enableUpdateDefaultSettings(bool enable) {
+        _updateDefaultSettings = enable;
+    }
 
     /////////////////////////////////////////////////////////////
     // the following functions have a default and minimalistic //
