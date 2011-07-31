92c2a2fcb2e764688664404474c671353b34da3a
Burkhard Lück <lueck@hube-lueck.de>
2011-07-31 20:37:07 +0200

    make the description translatable

--- krusader/main.cpp
+++ krusader/main.cpp
@@ -55,7 +55,7 @@
 #include "defaults.h"
 #include "Panel/krviewfactory.h"
 
-static const char *description = "Krusader\nTwin-Panel File Manager for KDE";
+static const char *description = I18N_NOOP("Krusader\nTwin-Panel File Manager for KDE");
 
 static void sigterm_handler(int i)
 {
