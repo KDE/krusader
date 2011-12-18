KDE/krusader

commit e7244ec462ec8fcceef9425182a33e42bbad22e7

remove KrusaderView::left/right - replace with leftPanel()/rightPanel()

krusader/krslots.cpp
@@ -166,14 +166,14 @@ void KRslots::compareContent()
     QStringList* lstActive;
     KUrl name1, name2;
 
-    MAIN_VIEW->left->getSelectedNames(&lstLeft);
-    MAIN_VIEW->right->getSelectedNames(&lstRight);
+    LEFT_PANEL->getSelectedNames(&lstLeft);
+    RIGHT_PANEL->getSelectedNames(&lstRight);
     lstActive = (ACTIVE_PANEL->gui->isLeft() ? &lstLeft : &lstRight);
 
     if (lstLeft.count() == 1 && lstRight.count() == 1) {
         // first, see if we've got exactly 1 selected file in each panel:
-        name1 = MAIN_VIEW->left->func->files()->vfs_getFile(lstLeft[0]);
-        name2 = MAIN_VIEW->right->func->files()->vfs_getFile(lstRight[0]);
+        name1 = LEFT_PANEL->func->files()->vfs_getFile(lstLeft[0]);
+        name2 = RIGHT_PANEL->func->files()->vfs_getFile(lstRight[0]);
     } else if (lstActive->count() == 2) {
         // next try: are in the current panel exacty 2 files selected?
         name1 = ACTIVE_PANEL->func->files()->vfs_getFile((*lstActive)[0]);
@@ -362,41 +362,16 @@ void KRslots::toggleHidden()
 
 void KRslots::swapPanels()
 {
-    KUrl leftURL = MAIN_VIEW->left->func->files()->vfs_getOrigin();
-    KUrl rightURL = MAIN_VIEW->right->func->files()->vfs_getOrigin();
+    KUrl leftURL = LEFT_PANEL->func->files()->vfs_getOrigin();
+    KUrl rightURL = RIGHT_PANEL->func->files()->vfs_getOrigin();
 
-    MAIN_VIEW->left->func->openUrl(rightURL);
-    MAIN_VIEW->right->func->openUrl(leftURL);
+    LEFT_PANEL->func->openUrl(rightURL);
+    LEFT_PANEL->func->openUrl(leftURL);
 }
 
 void KRslots::toggleSwapSides()
 {
-    QList<int> lst = MAIN_VIEW->horiz_splitter->sizes();
-
-    MAIN_VIEW->horiz_splitter->addWidget(MAIN_VIEW->leftMng);
-
-    int old = lst[ 0 ];
-    lst[ 0 ] = lst [ 1 ];
-    lst[ 1 ] = old;
-
-    MAIN_VIEW->horiz_splitter->setSizes(lst);
-
-    ListPanel     *tmpPanel;     // temporary variables for swapping
-    PanelManager  *tmpMng;
-
-    tmpMng = MAIN_VIEW->leftMng;
-    MAIN_VIEW->leftMng = MAIN_VIEW->rightMng;
-    MAIN_VIEW->rightMng = tmpMng;
-
-    tmpPanel = MAIN_VIEW->left;
-    MAIN_VIEW->left = MAIN_VIEW->right;
-    MAIN_VIEW->right = tmpPanel;
-
-    MAIN_VIEW->leftMng->swapPanels();
-    MAIN_VIEW->rightMng->swapPanels();
-
-    MAIN_VIEW->left->updateGeometry();
-    MAIN_VIEW->right->updateGeometry();
+    MAIN_VIEW->swapSides();
 }
 
 void KRslots::search()
@@ -512,8 +487,8 @@ void KRslots::rootKrusader()
 
     KProcess proc;
     proc << KrServices::fullPathName("kdesu") << KrServices::fullPathName("krusader")
-    + " --left=" + MAIN_VIEW->left->func->files()->vfs_getOrigin().url()
-    + " --right=" + MAIN_VIEW->right->func->files()->vfs_getOrigin().url();
+    + " --left=" + LEFT_PANEL->func->files()->vfs_getOrigin().url()
+    + " --right=" + RIGHT_PANEL->func->files()->vfs_getOrigin().url();
 
     if (!proc.startDetached())
         KMessageBox::error(0, i18n("Error executing %1!", proc.program()[0]));
@@ -780,8 +755,8 @@ void KRslots::manageUseractions()
 
 void KRslots::slotSynchronizeDirs(QStringList selected)
 {
-    new SynchronizerGUI(0, MAIN_VIEW->left->func->files()->vfs_getOrigin(),
-                        MAIN_VIEW->right->func->files()->vfs_getOrigin(), selected);
+    new SynchronizerGUI(0, LEFT_PANEL->func->files()->vfs_getOrigin(),
+                        RIGHT_PANEL->func->files()->vfs_getOrigin(), selected);
 }
 
 void KRslots::compareSetup()
@@ -823,8 +798,8 @@ void KRslots::slotQueueManager()
 void KRslots::windowActive()
 {
     if (MAIN_VIEW != 0) {  /* CRASH FIX: it's possible that the method is called after destroying the main view */
-        MAIN_VIEW->left->panelActive();
-        MAIN_VIEW->right->panelActive();
+        LEFT_PANEL->panelActive();
+        RIGHT_PANEL->panelActive();
     }
 }
 
@@ -833,8 +808,8 @@ void KRslots::windowActive()
 void KRslots::windowInactive()
 {
     if (MAIN_VIEW != 0) {  /* CRASH FIX: it's possible that the method is called after destroying the main view */
-        MAIN_VIEW->left->panelInactive();
-        MAIN_VIEW->right->panelInactive();
+        LEFT_PANEL->panelInactive();
+        RIGHT_PANEL->panelInactive();
     }
 }
 

krusader/Panel/krpanel.h
@@ -56,6 +56,7 @@ public:
     bool isLeft() {
         return _manager->isLeft();
     }
+    virtual void otherPanelChanged() {}
 
     ListPanel *gui;
     ListPanelFunc *func;

krusader/Panel/listpanel.h
@@ -88,6 +88,9 @@ public:
     // constructor create the panel, but DOESN'T fill it with data, use start()
     ListPanel(QWidget *parent, AbstractPanelManager *manager, KConfigGroup cfg = KConfigGroup());
     ~ListPanel();
+
+    virtual void otherPanelChanged();
+
     void start(KUrl url = KUrl(), bool immediate = false);
 
     int getType() {
@@ -116,8 +119,6 @@ public:
     void setProperties(int);
 
     void getFocusCandidates(QVector<QWidget*> &widgets);
-
-    void otherPanelChanged();
 
     void saveSettings(KConfigGroup cfg, bool localOnly, bool saveHistory = false);
     void restoreSettings(KConfigGroup cfg);

krusader/Synchronizer/synchronizergui.cpp
@@ -1951,10 +1951,9 @@ void SynchronizerGUI::closeDialog()
     this->deleteLater();
 
     if (wasSync) {
-        KrPanel *p = ACTIVE_PANEL;
-        MAIN_VIEW->left->func->refresh();
-        MAIN_VIEW->right->func->refresh();
-        p->gui->slotFocusOnMe();
+        LEFT_PANEL->func->refresh();
+        RIGHT_PANEL->func->refresh();
+        ACTIVE_PANEL->gui->slotFocusOnMe();
     }
 }
 

krusader/krglobal.cpp
@@ -51,7 +51,11 @@ KShortcut KrGlobal::copyShortcut;
 
 KrPanel *KrGlobal::activePanel()
 {
-    return mainView->activeManager()->currentPanel();
+    // active manager might not be set yet
+    if(mainView->activeManager())
+        return mainView->activeManager()->currentPanel();
+    else
+        return 0;
 }
 
 // void KrGlobal::enableAction(const char *name, bool enable)

krusader/krglobal.h
@@ -89,10 +89,10 @@ public:
 #define OTHER_MNG  (MAIN_VIEW->inactiveManager())
 #define OTHER_PANEL (ACTIVE_PANEL->otherPanel())
 #define OTHER_FUNC (OTHER_PANEL->func)
-#define LEFT_PANEL (MAIN_VIEW->left)
+#define LEFT_PANEL (MAIN_VIEW->leftPanel())
 #define LEFT_FUNC  (LEFT_PANEL->func)
 #define LEFT_MNG  (MAIN_VIEW->leftMng)
-#define RIGHT_PANEL  (MAIN_VIEW->right)
+#define RIGHT_PANEL  (MAIN_VIEW->rightPanel())
 #define RIGHT_FUNC (RIGHT_PANEL->func)
 #define RIGHT_MNG  (MAIN_VIEW->rightMng)
 

krusader/krusaderview.cpp
@@ -85,11 +85,8 @@ void KrusaderView::start(KConfigGroup &cfg, bool restoreSettings, QStringList le
     leftMng  = new PanelManager(horiz_splitter, true);
     rightMng = new PanelManager(horiz_splitter, false);
 
-    // now, create the panels inside the manager
-    KConfigGroup gl(krConfig, "Look&Feel");
-    int defaultType = gl.readEntry("Default Panel Type", KrViewFactory::defaultViewId());
-    left = leftMng->createPanel(defaultType);
-    right = rightMng->createPanel(defaultType);
+    // make the left panel focused at program start
+    activeMng = leftMng;
 
     // create the function keys widget
     fnKeys = new KFnKeys(this, krApp);
@@ -125,11 +122,8 @@ void KrusaderView::start(KConfigGroup &cfg, bool restoreSettings, QStringList le
         leftUrl = leftTabs[0];
     if(rightTabs.count())
         rightUrl = rightTabs[0];
-    right->start(rightUrl);
-    left->start(leftUrl);
-
-    // make the left panel focused at program start
-    activeMng = leftMng;
+    leftPanel()->start(leftUrl);
+    rightPanel()->start(rightUrl);
 
     ACTIVE_PANEL->gui->slotFocusOnMe();  // left starts out active
 
@@ -149,10 +143,20 @@ void KrusaderView::start(KConfigGroup &cfg, bool restoreSettings, QStringList le
         if(!rightTabs.count())
             rightMng->loadSettings(KConfigGroup(&cfg, "Right Tab Bar"));
         if (cfg.readEntry("Left Side Is Active", false))
-            left->slotFocusOnMe();
+            leftPanel()->slotFocusOnMe();
         else
-            right->slotFocusOnMe();
+            rightPanel()->slotFocusOnMe();
     }
+}
+
+ListPanel* KrusaderView::leftPanel()
+{
+    return leftMng->currentPanel()->gui;
+}
+
+ListPanel* KrusaderView::rightPanel()
+{
+    return rightMng->currentPanel()->gui;
 }
 
 // updates the command line whenever current panel or its path changes
@@ -231,6 +235,29 @@ void KrusaderView::slotSetActivePanel(ListPanel *p)
     else
         activeMng = rightMng;
     slotPathChanged(p);
+}
+
+void KrusaderView::swapSides()
+{
+    QList<int> lst = horiz_splitter->sizes();
+
+    horiz_splitter->addWidget(leftMng);
+
+    int old = lst[ 0 ];
+    lst[ 0 ] = lst [ 1 ];
+    lst[ 1 ] = old;
+
+    horiz_splitter->setSizes(lst);
+
+    PanelManager *tmpMng = leftMng;
+    leftMng = rightMng;
+    rightMng = tmpMng;
+
+    leftMng->setLeft(true);
+    rightMng->setLeft(false);
+
+    leftPanel()->updateGeometry();
+    rightPanel()->updateGeometry();
 }
 
 void KrusaderView::slotTerminalEmulator(bool show)
@@ -349,12 +376,12 @@ void KrusaderView::profiles(QString profileName)
 void KrusaderView::loadPanelProfiles(QString group)
 {
     KConfigGroup ldg(krConfig, group);
-    MAIN_VIEW->leftMng->loadSettings(KConfigGroup(&ldg, "Left Tabs"));
-    MAIN_VIEW->rightMng->loadSettings(KConfigGroup(&ldg, "Right Tabs"));
+    leftMng->loadSettings(KConfigGroup(&ldg, "Left Tabs"));
+    rightMng->loadSettings(KConfigGroup(&ldg, "Right Tabs"));
     if (ldg.readEntry("Left Side Is Active", true))
-        MAIN_VIEW->left->gui->slotFocusOnMe();
+        leftPanel()->slotFocusOnMe();
     else
-        MAIN_VIEW->right->gui->slotFocusOnMe();
+        rightPanel()->slotFocusOnMe();
 }
 
 void KrusaderView::savePanelProfiles(QString group)

krusader/krusaderview.h
@@ -70,7 +70,7 @@ public:
     void cmdLineFocus();  // command line receive's keyboard focus
     void cmdLineUnFocus();// return focus from command line to active panel
     inline bool isLeftActive() const {
-        return (ACTIVE_PANEL->gui == left);
+        return leftMng == activeMng;
     }
     inline PanelManager *activeManager() const {
         return activeMng;
@@ -78,10 +78,13 @@ public:
     inline PanelManager *inactiveManager() const {
         return activeMng == leftMng ? rightMng : leftMng;
     }
+    ListPanel* leftPanel();
+    ListPanel* rightPanel();
     QList<int> getTerminalEmulatorSplitterSizes();
     inline bool          isVertical() const {
         return horiz_splitter != 0 ? horiz_splitter->orientation() == Qt::Vertical : false;
     }
+    void swapSides();
 
 public slots:
     void slotSetActivePanel(ListPanel *p);
@@ -106,7 +109,6 @@ public slots:
     void killTerminalEmulator();
 
 public:
-    ListPanel  *left, *right;       // the actual panels
     PanelManager *activeMng, *leftMng, *rightMng;       // saving them for panel swaps
     KFnKeys   *fnKeys;          // function keys
     KCMDLine    *cmdLine;                   // command line widget

commit d95c23dc517391b6a95e7a95369d467ec231c937

FIXED: [ 277838 ] Swap Panels no longer works bug was introduced by e7244ec462ec8fcceef9425182a33e42bbad22e7

krusader/krslots.cpp
@@ -347,7 +347,7 @@ void KRslots::swapPanels()
     KUrl rightURL = RIGHT_PANEL->func->files()->vfs_getOrigin();
 
     LEFT_PANEL->func->openUrl(rightURL);
-    LEFT_PANEL->func->openUrl(leftURL);
+    RIGHT_PANEL->func->openUrl(leftURL);
 }
 
 void KRslots::toggleSwapSides()