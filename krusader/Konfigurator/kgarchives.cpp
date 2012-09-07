0ab7ccd6bdbe077a296495fd5ab5b591151f6747
Pino Toscano <pino@kde.org>
2012-09-07 16:56:15 +0200

    i18n: improve message

--- krusader/Konfigurator/kgarchives.cpp
+++ krusader/Konfigurator/kgarchives.cpp
@@ -118,7 +118,7 @@ KgArchives::KgArchives(bool first, QWidget* parent) :
              "<b>Failure during the process might result in data loss.</b><br/>"
              ),
         krarcGrp), 2, 0);
-    krarcGrid->addWidget(new QLabel(i18n("<b>Moving archives into themself will delete them !</b>"), krarcGrp), 3, 0);
+    krarcGrid->addWidget(new QLabel(i18n("<b>Moving archives into themselves will delete them.</b>"), krarcGrp), 3, 0);
 
     kgArchivesLayout->addWidget(krarcGrp, 1 , 0);
 
