aeded6b14a8c4c1a9afa0e60cfd45f4b98d48d46
Jan Lepper <jan_lepper@gmx.de>
2012-09-07 17:10:23 +0200

    formatting

--- krusader/Konfigurator/kgarchives.cpp
+++ krusader/Konfigurator/kgarchives.cpp
@@ -113,11 +113,10 @@ KgArchives::KgArchives(bool first, QWidget* parent) :
     KonfiguratorCheckBoxGroup *krarcCheckBoxes = createCheckBoxGroup(1, 0, krarcOptions, 1, krarcGrp);
 
     krarcGrid->addWidget(krarcCheckBoxes, 1, 0);
-    krarcGrid->addWidget(new QLabel(
-        i18n("<b>Caution when moving into archives:</b><br/>"
-             "<b>Failure during the process might result in data loss.</b><br/>"
-             ),
-        krarcGrp), 2, 0);
+    krarcGrid->addWidget(
+            new QLabel(i18n("<b>Caution when moving into archives:</b><br/>"
+                            "<b>Failure during the process might result in data loss.</b><br/>"), krarcGrp),
+            2, 0);
     krarcGrid->addWidget(new QLabel(i18n("<b>Moving archives into themselves will delete them.</b>"), krarcGrp), 3, 0);
 
     kgArchivesLayout->addWidget(krarcGrp, 1 , 0);
