f2b9b117d3e2f7cfcd802f03f4cd7a26fccb0611
Jan Lepper <jan_lepper@gmx.de>
2012-09-25 14:01:44 +0200

    FIXED: [ 300514 ] no warning if terminal emulator can't be created BUG: 300514

--- krusader/GUI/terminaldock.cpp
+++ krusader/GUI/terminaldock.cpp
@@ -35,6 +35,7 @@
 #include <kservice.h>
 #include <ktoggleaction.h>
 #include <kurl.h>
+#include <kmessagebox.h>
 
 #include "../kractions.h"
 #include "../krusaderapp.h"
@@ -63,16 +64,13 @@ TerminalDock::~TerminalDock()
 bool TerminalDock::initialise()
 {
     if (! initialised) { // konsole part is not yet loaded or it has already failed
-        KPluginFactory* factory = NULL;
         KService::Ptr service = KService::serviceByDesktopName("konsolepart");
-        if( service ) {
-            factory = KPluginLoader(service->library()).factory();
-        }
 
-        if (factory) {
+        if (service) {
             QWidget *focusW = qApp->focusWidget();
             // Create the part
-            konsole_part = factory->create<KParts::ReadOnlyPart>((QObject *)this);
+            QString error;
+            konsole_part = service->createInstance<KParts::ReadOnlyPart>(this, this, QVariantList(), &error);
 
             if (konsole_part) { //loaded successfully
                 terminal_hbox->addWidget(konsole_part->widget());
@@ -88,7 +86,10 @@ bool TerminalDock::initialise()
                     lastPath = QDir::currentPath();
                     t->showShellInDir(lastPath);
                 }
-            }
+                initialised = true;
+            } else
+                KMessageBox::error(0, i18n("<b>Cannot create embedded terminal.</b><br/>"
+                                           "The reported error was: %1", error));
             // the Terminal Emulator may be hidden (if we are creating it only
             // to send command there and see the results later)
             if (focusW) {
@@ -96,8 +97,13 @@ bool TerminalDock::initialise()
             } else {
                 ACTIVE_PANEL->gui->slotFocusOnMe();
             }
-        }
-        initialised = true;
+        } else
+            KMessageBox::sorry(0, i18nc("missing program - arg1 is a URL",
+                                        "<b>Cannot create embedded terminal.</b><br>"
+                                        "You can fix this by installing Konsole:<br/>%1",
+                                        QString("<a href='%1'>%1</a>").arg(
+                                            "http://www.kde.org/applications/system/konsole")),
+                               0, KMessageBox::AllowLink);
     }
     return isInitialised();
 }
