c1eddb51781830c5f0b80b583ba3ee057d0b5ce3
Jan Lepper <jan_lepper@gmx.de>
2013-02-28 00:34:42 +0100

    add retired flag to author roles in about dialog

--- krusader/main.cpp
+++ krusader/main.cpp
@@ -136,18 +136,18 @@ int main(int argc, char *argv[])
 
     aboutData.setProgramIconName(Krusader::privIcon());
 
-    aboutData.addAuthor(ki18n("Rafi Yanai"), ki18n("Author"), "yanai@users.sourceforge.net");
-    aboutData.addAuthor(ki18n("Shie Erlich"), ki18n("Author"), "erlich@users.sourceforge.net");
-    aboutData.addAuthor(ki18n("Karai Csaba"), ki18n("Developer"), "ckarai@users.sourceforge.net", 0);
-    aboutData.addAuthor(ki18n("Heiner Eichmann"), ki18n("Developer"), "h.eichmann@gmx.de", 0);
-    aboutData.addAuthor(ki18n("Jonas Bähr"), ki18n("Developer"), "jonas.baehr@web.de", 0);
-    aboutData.addAuthor(ki18n("Václav Jůza"), ki18n("Developer"), "vaclavjuza@gmail.com", 0);
+    aboutData.addAuthor(ki18n("Rafi Yanai"), ki18n("Author (retired)"), "yanai@users.sourceforge.net");
+    aboutData.addAuthor(ki18n("Shie Erlich"), ki18n("Author (retired)"), "erlich@users.sourceforge.net");
     aboutData.addAuthor(ki18n("Jan Lepper"), ki18n("Developer"), "jan_lepper@gmx.de", 0);
     aboutData.addAuthor(ki18n("Andrey Matveyakin"), ki18n("Developer"), "a.matveyakin@gmail.com", 0);
-    aboutData.addAuthor(ki18n("Dirk Eschler"), ki18n("Webmaster and i18n coordinator"), "deschler@users.sourceforge.net", 0);
-    aboutData.addAuthor(ki18n("Frank Schoolmeesters"), ki18n("Documentation and marketing coordinator"), "frank_schoolmeesters@yahoo.com", 0);
-    aboutData.addAuthor(ki18n("Richard Holt"), ki18n("Documentation & Proofing"), "richard.holt@gmail.com", 0);
-    aboutData.addAuthor(ki18n("Matej Urbancic"), ki18n("Marketing & Product Research"), "matej.urban@gmail.com", 0);
+    aboutData.addAuthor(ki18n("Karai Csaba"), ki18n("Developer (retired)"), "ckarai@users.sourceforge.net", 0);
+    aboutData.addAuthor(ki18n("Heiner Eichmann"), ki18n("Developer (retired)"), "h.eichmann@gmx.de", 0);
+    aboutData.addAuthor(ki18n("Jonas Bähr"), ki18n("Developer (retired)"), "jonas.baehr@web.de", 0);
+    aboutData.addAuthor(ki18n("Václav Jůza"), ki18n("Developer (retired)"), "vaclavjuza@gmail.com", 0);
+    aboutData.addAuthor(ki18n("Dirk Eschler"), ki18n("Webmaster"), "deschler@users.sourceforge.net", 0);
+    aboutData.addAuthor(ki18n("Frank Schoolmeesters"), ki18n("Documentation and marketing coordinator (retired)"), "frank_schoolmeesters@yahoo.com", 0);
+    aboutData.addAuthor(ki18n("Richard Holt"), ki18n("Documentation & Proofing (retired)"), "richard.holt@gmail.com", 0);
+    aboutData.addAuthor(ki18n("Matej Urbancic"), ki18n("Marketing & Product Research (retired)"), "matej.urban@gmail.com", 0);
     aboutData.addCredit(ki18n("kde.org"), ki18n("Everyone involved in KDE"), 0, 0);
     aboutData.addCredit(ki18n("l10n.kde.org"), ki18n("KDE Translation Teams"), 0, 0);
     aboutData.addCredit(ki18n("Jiří Paleček"), ki18n("QA, bug-hunting, patches and general help"), "jpalecek@web.de", 0);
