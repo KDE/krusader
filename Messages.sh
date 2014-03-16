07831980e8de30f4b0fe53c14d7c8087eb22af43
Pino Toscano <pino@kde.org>
2014-03-16 17:11:52 +0100

    remove old tar kioslave
    Is is a copy of the kio_archive in kde-runtime, and this copy does not
    even build (not even done with the porting from qt3/kde3).

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-dirs="krusader krArc tar iso"
+dirs="krusader krArc iso"
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
 $XGETTEXT rc.cpp `find $dirs -name \*.cpp -o -name \*.cc -o -name \*.c -o -name \*.h` -o $podir/krusader.pot
 rm -f rc.cpp
