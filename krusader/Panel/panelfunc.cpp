KDE/krusader

commit ac4d5e675bb8731e999cfaa40820793d69a7e637

Get default file protocols from KIO instead of hardcoding
Was about to add .tar.xz to the list of hardcoded protocols but decided to change the logic to check which protocol handlers are installed in the system instead.
Now the Protocols konfigurator page is just used for overrides, not for defaults. This way more file formats can be supported just by installing more kio slaves, no need to change krusader code to add support.

Also change how the detection works for the case of user navigating up, outside the archive file. Use the ":local" protocol class to detect generally when user was loooking inside of a file.
Another change is to not "do nothing" in case user tries to enter an archive file which does not have the needed program to decode. Instead, just try it and see if it works. If not then the user will at least see that krusader registered the user's command to open and will then see an error message why it did not work.

krusader/Panel/panelfunc.cpp
@@ -50,6 +50,7 @@ A
 #include <KI18n/KLocalizedString>
 #include <KIO/DesktopExecParser>
 #include <KIO/JobUiDelegate>
+#include <KIOCore/KProtocolInfo>
 #include <KIOWidgets/KOpenWithDialog>
 #include <KIOWidgets/KPropertiesDialog>
 #include <KIOWidgets/KRun>
@@ -252,7 +253,8 @@ void ListPanelFunc::doRefresh()
             tmp = history->currentUrl().fileName();
             history->goForward();
             QUrl t = history->currentUrl();
-            if (t.scheme() == "krarc" || t.scheme() == "tar" || t.scheme() == "iso") {
+            QString protocol = t.scheme();
+            if (protocol != QStringLiteral("file") && KProtocolInfo::protocolClass(protocol).toLower() == QStringLiteral(":local")) {
                 t.setScheme("file");
                 history->setCurrentUrl(t);
                 panel->vfsError->hide();
@@ -920,35 +922,17 @@ void ListPanelFunc::goInside(const QString& name)
     if (vf == 0)
         return ;
 
-    QUrl origin = files() ->vfs_getOrigin();
     QUrl url = vf->vfile_getUrl();
 
-    if (vf->vfile_isDir()) {               // we create a return-pressed event,
-        execute(name); // thereby emulating a chdir
+    if (vf->vfile_isDir()) {
+        panel->view->setNameToMakeCurrent(QString());
+        openUrl(url);
     } else if (url.isLocalFile()) {
-        bool encrypted;
         QString mime = vf->vfile_getMime();
-        QString type = KRarcHandler::getType(encrypted, url.path(), mime, false, true);
-
-        if (KRarcHandler::arcSupported(type)) {    // archive autodetection
-            // here we check whether KDE supports tar
-            if (type == "-tlz") {
-                KTar tapeArchive(url.path());
-                if (tapeArchive.open(QIODevice::ReadOnly))
-                    url.setScheme("tar");
-                else
-                    url.setScheme("krarc");
-            } else if (type == "-tar" || type == "-tgz" || type == "-tbz")
-                url.setScheme("tar");
-            else
-                url.setScheme("krarc");
+        QString protocol = KrServices::registeredProtocol(mime);
+        if(!protocol.isEmpty()) {
+            url.setScheme(protocol);
             openUrl(url);
-        } else {
-            QString protocol = KrServices::registerdProtocol(mime);
-            if (protocol == "iso") {
-                url.setScheme(protocol);
-                openUrl(url);
-            }
         }
     }
 }
@@ -993,31 +977,17 @@ void ListPanelFunc::execute(const QString& name)
         return ;
     }
 
-    krOut<<name<<endl;
-
     vfile *vf = files() ->vfs_search(name);
     if (vf == 0)
         return ;
 
-    QUrl origin = files() ->vfs_getOrigin();
-
-    QString protocol = origin.isLocalFile() ? KrServices::registerdProtocol(vf->vfile_getMime()) : "";
-
-    if (protocol == "tar" || protocol == "krarc") {
-        bool encrypted;
-        QString type = KRarcHandler::getType(encrypted, vf->vfile_getUrl().path(), vf->vfile_getMime(), false, true);
-        if (!KRarcHandler::arcHandled(type))       // if the specified archive is disabled delete the protocol
-            protocol = "";
+    QString protocol;
+    if(files()->vfs_getOrigin().isLocalFile()) {
+        protocol = KrServices::registeredProtocol(vf->vfile_getMime());
     }
 
-    if (vf->vfile_isDir()) {
-        origin = files() ->vfs_getFile(name);
-        panel->view->setNameToMakeCurrent(QString());
-        openUrl(origin);
-    } else if (!protocol.isEmpty()) {
-        QUrl path = files() ->vfs_getFile(vf->vfile_getName());
-        path.setScheme(protocol);
-        openUrl(path);
+    if (vf->vfile_isDir() || !protocol.isEmpty()) {
+        goInside(name);
     } else {
         QUrl url = files() ->vfs_getFile(name);
         if (KRun::isExecutableFile(url, vf->vfile_getMime()))

iso/iso.protocol
@@ -1,12 +1,15 @@
 [Protocol]
 exec=kio_iso
 protocol=iso
-listing=Name,Type,Size,Date,AccessDate,CreationDate,Access,Owner,Group,Link
+archiveMimetype=application/x-iso,application/x-cd-image,application/x-dvd-image
 input=filesystem
 output=filesystem
+listing=Name,Type,Size,Date,AccessDate,CreationDate,Access,Owner,Group,Link
 reading=true
 source=true
 Icon=media-optical
+Class=:local
+
 Description=A kioslave for ISO9660 filesystems
 Description[bg]=Поддръжка на файлови системи ISO9660
 Description[bs]=Kioslave za ISO9660 datotečne sisteme
@@ -46,4 +49,3 @@ Description[uk]=Kioslave для файлових систем ISO9660
 Description[x-test]=xxA kioslave for ISO9660 filesystemsxx
 Description[zh_CN]=ISO9660 文件系统的 kioslave
 Description[zh_TW]=ISO9660 檔案系統的 kioslave
-MimeType=application/x-iso

krArc/krarc.protocol
@@ -1,7 +1,7 @@
 [Protocol]
 exec=kio_krarc
 protocol=krarc
-mimetype=application/x-archive
+archiveMimetype=application/x-7z,application/x-7z-compressed,application/x-ace,application/x-ace-compressed,application/x-arj,application/x-arj-compressed,application/x-cpio,application/x-deb,application/x-debian-package,application/vnd.debian.binary-package,application/x-java-archive,application/x-lha,application/x-lha-compressed,application/x-rar,application/x-rar-compressed,application/x-rpm,application/x-source-rpm,application/zip,application/x-zip,application/x-zip-compressed
 input=filesystem
 output=filesystem
 listing=Name,Type,Size,Date,Access,Owner,Group,Link
@@ -13,7 +13,9 @@ source=true
 linking=false
 moving=false
 copyToFile=true
-Icon=ark
+Icon=package-x-generic
+Class=:local
+
 Description=Mutiple archive handler that currently supports: zip,rar.
 Description[bg]=Поддръжка на множество архиви (в момента се поддържат zip и rar).
 Description[bs]=Rukovatelj arhivama koji trenutno podržava: zip,rar.

krusader/Konfigurator/kgprotocols.cpp
@@ -43,28 +43,6 @@
 #include <KIOCore/KProtocolInfo>
 #include <KIconThemes/KIconLoader>
 
-QString KgProtocols::defaultProtocols  = "krarc,iso,tar";
-QString KgProtocols::defaultIsoMimes   = "application/x-iso,application/x-cd-image,"
-        "application/x-dvd-image";
-QString KgProtocols::defaultKrarcMimes = "application/x-7z,application/x-7z-compressed,"
-        "application/x-ace,application/x-ace-compressed,"
-        "application/x-arj,application/x-arj-compressed,"
-        "application/x-bzip2,"
-        "application/x-cpio,application/x-deb,"
-        "application/x-debian-package,"
-        "application/x-gzip,application/x-jar,"
-        "application/x-lha,application/x-lha-compressed,"
-        "application/x-rar,application/x-rar-compressed,"
-        "application/x-xz,"
-        "application/x-rpm,application/zip,"
-        "application/x-source-rpm,"
-        "application/x-zip,application/x-zip-compressed";
-QString KgProtocols::defaultTarMimes   = "application/x-tar,application/x-tarz,"
-        "application/x-bzip-compressed-tar,"
-        "application/x-compressed-tar,"
-        "application/x-tbz,application/x-tgz,"
-        "application/x-xz-compressed-tar";
-
 KgProtocols::KgProtocols(bool first, QWidget* parent) :
         KonfiguratorPage(first, parent)
 {
@@ -351,21 +329,6 @@ void KgProtocols::setDefaults()
     while (linkList->topLevelItemCount() != 0)
         removeProtocol(linkList->topLevelItem(0)->text(0));
 
-    addProtocol("iso");
-    QStringList isoMimes = defaultIsoMimes.split(',');
-    for (QStringList::Iterator it = isoMimes.begin(); it != isoMimes.end(); it++)
-        addMime(*it, "iso");
-#ifdef KRARC_ENABLED
-    addProtocol("krarc");
-    QStringList krarcMimes = defaultKrarcMimes.split(',');
-    for (QStringList::Iterator it = krarcMimes.begin(); it != krarcMimes.end(); it++)
-        addMime(*it, "krarc");
-#endif
-    addProtocol("tar");
-    QStringList tarMimes = defaultTarMimes.split(',');
-    for (QStringList::Iterator it = tarMimes.begin(); it != tarMimes.end(); it++)
-        addMime(*it, "tar");
-
     slotDisableButtons();
 
     if (isChanged())
@@ -429,16 +392,5 @@ bool KgProtocols::apply()
 
     emit sigChanged();
     return false;
-}
-
-void KgProtocols::init()
-{
-    if (!krConfig->groupList().contains("Protocols")) {
-        KConfigGroup group(krConfig, "Protocols");
-        group.writeEntry("Handled Protocols", defaultProtocols);
-        group.writeEntry("Mimes For iso",     defaultIsoMimes);
-        group.writeEntry("Mimes For krarc",   defaultKrarcMimes);
-        group.writeEntry("Mimes For tar",     defaultTarMimes);
-    }
 }
 

krusader/Konfigurator/kgprotocols.h
@@ -49,8 +49,6 @@ public:
     virtual bool apply() Q_DECL_OVERRIDE;
     virtual bool isChanged() Q_DECL_OVERRIDE;
 
-    static  void init();
-
 public slots:
     void         slotDisableButtons();
     void         slotAddProtocol();

krusader/krservices.cpp
@@ -23,6 +23,7 @@
 #include <QtCore/QTextStream>
 
 #include <KConfigCore/KSharedConfig>
+#include <KIOCore/KProtocolManager>
 
 #include "krglobal.h"
 #include "defaults.h"
@@ -76,7 +77,7 @@ QString KrServices::chooseFullPathName(QStringList names, QString confName)
     return "";
 }
 
-QString KrServices::registerdProtocol(QString mimetype)
+QString KrServices::registeredProtocol(QString mimetype)
 {
     if (slaveMap == 0) {
         slaveMap = new QMap<QString, QString>();
@@ -88,10 +89,12 @@ QString KrServices::registerdProtocol(QString mimetype)
             for (QStringList::Iterator it2 = mimes.begin(); it2 != mimes.end(); it2++)
                 (*slaveMap)[*it2] = *it;
         }
-
-
     }
-    return (*slaveMap)[mimetype];
+    QString protocol = (*slaveMap)[mimetype];
+    if(protocol.isEmpty()) {
+        protocol = KProtocolManager::protocolForArchiveMimetype(mimetype);
+    }
+    return protocol;
 }
 
 void KrServices::clearProtocolCache()

krusader/krservices.h
@@ -37,7 +37,7 @@ public:
     static bool         cmdExist(QString cmdName);
     static QString      chooseFullPathName(QStringList names, QString confName);
     static QString      fullPathName(QString name, QString confName = QString());
-    static QString      registerdProtocol(QString mimetype);
+    static QString      registeredProtocol(QString mimetype);
     static QString      getPath(const QUrl &url, QUrl::FormattingOptions options = QUrl::PrettyDecoded);
     static void         clearProtocolCache();
     static bool         fileToStringList(QTextStream *stream, QStringList& target, bool keepEmptyLines = false);

krusader/krusader.cpp
@@ -167,9 +167,6 @@ Krusader::Krusader(const QCommandLineParser &parser) : KParts::MainWindow(0,
     // init the permmision handler class
     KRpermHandler::init();
 
-    // init the protocol handler
-    KgProtocols::init();
-
     // init the checksum tools
     initChecksumModule();
 

commit 014da219e0041900c0b59099c1132d59f781e6fb

fix browsing from / directory
when in / you can't cd into any directory pressing return as vfile_getUrl return a bad QUrl (file://bin) whereas vfs_getFile(QString) does it correctly
(file:///bin)

the regression was introduced with commit ac4d5e675bb8731e999cfaa40820793d69a7e637 and the bug shows up only in / directory (no need to change vfs at
all)

krusader/Panel/panelfunc.cpp
@@ -925,6 +925,7 @@ void ListPanelFunc::goInside(const QString& name)
     QUrl url = vf->vfile_getUrl();
 
     if (vf->vfile_isDir()) {
+        url = files()->vfs_getFile(name);
         panel->view->setNameToMakeCurrent(QString());
         openUrl(url);
     } else if (url.isLocalFile()) {