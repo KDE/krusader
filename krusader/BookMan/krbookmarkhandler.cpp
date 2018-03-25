KDE/krusader

commit f515bfa6b40ac7d18a50e7abe2d34f1e3ef1b262

Fixed jump-back actions in bookmark menu, improved separators
This fixes the issue when bookmark search is started with 's' or 'j'.
Previously these letter were accelerators coming from Go global menu,
which prevents from searching bookmarks started from the letters.
Now, the bookmark menu has a dedicated actions without the accelerators.

krusader/BookMan/krbookmarkhandler.cpp
@@ -434,7 +434,7 @@ void KrBookmarkHandler::buildMenu(KrBookmark *parent, QMenu *menu, int depth)
 
         // do we need to add special bookmarks?
         if (SPECIAL_BOOKMARKS) {
-            if (hasTrash || hasLan || hasVirtualFS || hasJumpback)
+            if (hasTrash || hasLan || hasVirtualFS)
                 menu->addSeparator();
 
             KrBookmark *bm;
@@ -462,21 +462,23 @@ void KrBookmarkHandler::buildMenu(KrBookmark *parent, QMenu *menu, int depth)
             }
 
             if (hasJumpback) {
-                // add the jump-back button
+                menu->addSeparator();
+
                 ListPanelActions *actions = _mainWindow->listPanelActions();
-                if (actions) {
-                    menu->addAction(actions->actJumpBack);
-                    _specialBookmarks.append(actions->actJumpBack);
-                    menu->addSeparator();
-                    menu->addAction(actions->actSetJumpBack);
-                    _specialBookmarks.append(actions->actSetJumpBack);
+
+                auto action = KrBookmark::jumpBackAction(_collection, false, actions);
+                if (action) {
+                    menu->addAction(action);
+                }
+
+                action = KrBookmark::jumpBackAction(_collection, true, actions);
+                if (action) {
+                    menu->addAction(action);
                 }
             }
         }
 
-        if (!hasJumpback)
-            menu->addSeparator();
-
+        menu->addSeparator();
         menu->addAction(KrActions::actAddBookmark);
         _specialBookmarks.append(KrActions::actAddBookmark);
         QAction *bmAct = menu->addAction(krLoader->loadIcon("bookmarks", KIconLoader::Small),

krusader/BookMan/krbookmark.cpp
@@ -21,6 +21,7 @@
 #include "../krglobal.h"
 #include "../Archive/krarchandler.h"
 #include "../FileSystem/krtrashhandler.h"
+#include "../Panel/listpanelactions.h"
 
 #include <KI18n/KLocalizedString>
 #include <KIconThemes/KIconLoader>
@@ -71,12 +72,12 @@ KrBookmark::~KrBookmark()
     }
 }
 
-KrBookmark* KrBookmark::getExistingBookmark(QString actionName, KActionCollection *collection)
+KrBookmark * KrBookmark::getExistingBookmark(QString actionName, KActionCollection *collection)
 {
     return static_cast<KrBookmark*>(collection->action(BM_NAME(actionName)));
 }
 
-KrBookmark* KrBookmark::trash(KActionCollection *collection)
+KrBookmark * KrBookmark::trash(KActionCollection *collection)
 {
     KrBookmark *bm = getExistingBookmark(i18n(NAME_TRASH), collection);
     if (!bm)
@@ -86,7 +87,7 @@ KrBookmark* KrBookmark::trash(KActionCollection *collection)
     return bm;
 }
 
-KrBookmark* KrBookmark::virt(KActionCollection *collection)
+KrBookmark * KrBookmark::virt(KActionCollection *collection)
 {
     KrBookmark *bm = getExistingBookmark(i18n(NAME_VIRTUAL), collection);
     if (!bm) {
@@ -96,7 +97,7 @@ KrBookmark* KrBookmark::virt(KActionCollection *collection)
     return bm;
 }
 
-KrBookmark* KrBookmark::lan(KActionCollection *collection)
+KrBookmark * KrBookmark::lan(KActionCollection *collection)
 {
     KrBookmark *bm = getExistingBookmark(i18n(NAME_LAN), collection);
     if (!bm) {
@@ -106,7 +107,30 @@ KrBookmark* KrBookmark::lan(KActionCollection *collection)
     return bm;
 }
 
-KrBookmark* KrBookmark::separator()
+QAction * KrBookmark::jumpBackAction(KActionCollection *collection, bool isSetter, ListPanelActions *sourceActions)
+{
+    auto actionName = isSetter ? QString("setJumpBack") : QString("jumpBack");
+    auto action = collection->action(actionName);
+    if (action) {
+        return action;
+    }
+
+    if (!sourceActions) {
+        return nullptr;
+    }
+
+    // copy essential part of source action (shortcut is not copied as it will introduce ambiguity)
+    auto sourceAction = isSetter ? sourceActions->actSetJumpBack : sourceActions->actJumpBack;
+    action = new QAction(sourceAction->icon(), sourceAction->text(), sourceAction);
+    connect(action, &QAction::triggered, sourceAction, &QAction::trigger);
+    // ensure there are no accelerator keys coming from another menu
+    action->setText(KLocalizedString::removeAcceleratorMarker(action->text()));
+
+    collection->addAction(actionName, action);
+    return action;
+}
+
+KrBookmark * KrBookmark::separator()
 {
     KrBookmark *bm = new KrBookmark("");
     bm->_separator = true;

krusader/BookMan/krbookmark.h
@@ -26,8 +26,9 @@
 // QtWidgets
 #include <QAction>
 
-
 class KActionCollection;
+class ListPanelActions;
+
 
 class KrBookmark: public QAction
 {
@@ -57,12 +58,14 @@ public:
         return _children;
     }
 
-    static KrBookmark* getExistingBookmark(QString actionName, KActionCollection *collection);
+    static KrBookmark * getExistingBookmark(QString actionName, KActionCollection *collection);
+
     // ----- special bookmarks
-    static KrBookmark* trash(KActionCollection *collection);
-    static KrBookmark* virt(KActionCollection *collection);
-    static KrBookmark* lan(KActionCollection *collection);
-    static KrBookmark* separator();
+    static KrBookmark * trash(KActionCollection *collection);
+    static KrBookmark * virt(KActionCollection *collection);
+    static KrBookmark * lan(KActionCollection *collection);
+    static QAction * jumpBackAction(KActionCollection *collection, bool isSetter = false, ListPanelActions *sourceActions = 0);
+    static KrBookmark * separator();
 
 signals:
     void activated(const QUrl &url);

commit d17d5fb4ddf1397fc228532317b17a6d1aa54394

Add back Jump Back actions to special bookmarks
Fixed regression introduced in f515bfa:
Jump Back actions should belong to special bookmarks
for right click on the actions to display popup menu instead of
triggering the actions.

Authored By: rade
Reviewed By: nmel, martinkostolny, #krusader

Differential Revision: https://phabricator.kde.org/D11623

krusader/BookMan/krbookmarkhandler.cpp
@@ -474,11 +474,13 @@ void KrBookmarkHandler::buildMenu(KrBookmark *parent, QMenu *menu, int depth)
                 auto action = KrBookmark::jumpBackAction(_privateCollection, true, actions);
                 if (action) {
                     menu->addAction(action);
+                    _specialBookmarks.append(action);
                 }
 
                 action = KrBookmark::jumpBackAction(_privateCollection, false, actions);
                 if (action) {
                     menu->addAction(action);
+                    _specialBookmarks.append(action);
                 }
             }
         }