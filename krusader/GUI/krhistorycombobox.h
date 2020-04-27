9d6139bbd8d004383b601f1aad787e9bb78bca2a
Toni Asensi Esteve <toni.asensi@kdemail.net>
2020-04-28 00:29:45 +0200

    Add a constructor (similar to a KHistoryComboBox one) to the KrHistoryComboBox class
    Add a constructor (similar to a KHistoryComboBox one) to
    the KrHistoryComboBox class, which will come handy for the next replacements
    of KHistoryComboBoxes with KrHistoryComboBoxes.
    
    Differential Revision: https://phabricator.kde.org/D28961

--- krusader/GUI/krhistorycombobox.h
+++ krusader/GUI/krhistorycombobox.h
@@ -34,6 +34,7 @@ class KrHistoryComboBox : public KHistoryComboBox
 
 public:
     explicit KrHistoryComboBox(bool useCompletion, QWidget *parent = nullptr);
+    explicit KrHistoryComboBox(QWidget *parent = nullptr) : KrHistoryComboBox(true, parent) {}
 };
 
 #endif // KRHISTORYCOMBOBOX_H
