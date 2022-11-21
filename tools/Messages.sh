ce1e49bf51deee72045476b4b2f5a1423e5ce289
Albert Astals Cid <aacid@kde.org>
2022-11-22 00:22:52 +0100

    Fix problem due to Messages.sh file being in strange location
    Discussion: https://invent.kde.org/utilities/krusader/-/merge_requests/115
    (cherry picked from commit f1ccf3b99a5593854a2b4a3c4991e487d9d0a49d)

--- tools/Messages.sh
+++ tools/Messages.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 cd ..
 dirs="app plugins/iso plugins/krarc"
+
+# This is usually added by scripty but the fact that this Messages.sh file
+# is hidden away in subdir that doesn't contain the actual code breaks scripty's heuristic
+echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> rc.cpp
+echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> rc.cpp
+
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
 $EXTRACTRC --tag-group=none --tag=title --tag=category --tag=tooltip --tag=description app/useraction_examples.xml >> rc.cpp
 $XGETTEXT rc.cpp `find $dirs -name \*.cpp -o -name \*.cc -o -name \*.c -o -name \*.h` -o $podir/krusader.pot
