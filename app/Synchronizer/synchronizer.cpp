085753c5332ccce70d6003c711463e46e554ac10
Toni Asensi Esteve <toni.asensi@kdemail.net>
2024-08-03 21:04:46 +0200

    Synchronizer: Make 'Copy from right to left' the default action if 'Asymmetric' and 'Ignore Date' are activated
    Make "Copy from right to left" the default action in Synchronizer if those
    options are checked: "Asymmetric" and "Ignore Date".
    
    Credits for Alex Bikadorov.
    
    FIXED: [ 476194 ] In more cases, the default behavior in Synchronizer should be to overwrite files
    BUG: 476194
    
    Revision: https://invent.kde.org/utilities/krusader/-/merge_requests/138

--- app/Synchronizer/synchronizer.cpp
+++ app/Synchronizer/synchronizer.cpp
@@ -650,22 +650,21 @@ SynchronizerFileItem *Synchronizer::addDuplicateItem(SynchronizerFileItem *paren
             }
         }
 
-        if (ignoreDate)
-            task = TT_DIFFERS;
-        else if (asymmetric) {
+        if (asymmetric) {
             // To avoid that the default action is
             // to overwrite a file by an old version of it
-            if (leftDate > checkedRightDate)
+            if (!ignoreDate && leftDate > checkedRightDate)
                 task = TT_DIFFERS;
             else
                 task = TT_COPY_TO_LEFT;
-        } else if (leftDate > checkedRightDate)
+        } else if (ignoreDate)
+            task = TT_DIFFERS;
+        else if (leftDate > checkedRightDate)
             task = TT_COPY_TO_RIGHT;
         else if (leftDate < checkedRightDate)
             task = TT_COPY_TO_LEFT;
         else
             task = TT_DIFFERS;
-
     } while (false);
 
     SynchronizerFileItem *item = addItem(parent,
